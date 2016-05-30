ab2c7f045a706951ce409f213b7a19f3f8ba0126
jason <12481228@qq.com>
2016-05-30 18:57:56 +0800

    解决跨越问题：代码优化

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/filter/CORSFilter.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/filter/CORSFilter.java
@@ -1,40 +1,45 @@
+/*************************************************
+ * @功能简述: 拦截器，用来解决跨越问题
+ * @项目名称: marketing cloud
+ * @see: 
+ * @author: 宋世涛
+ * @version: 0.0.1
+ * @date: 2016/5/30
+ * @复审人: 
+*************************************************/
+
 package cn.rongcapital.mkt.common.filter;
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter {
 
+	@Value("${api.cors.domain}")
+	private String CORSDomain;
+	private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc)
 			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
 	    HttpServletResponse response = (HttpServletResponse) res;
-	    response.setHeader("Access-Control-Allow-Origin", "*");
+	    response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, CORSDomain);
 	    fc.doFilter(req, res);
-		
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
