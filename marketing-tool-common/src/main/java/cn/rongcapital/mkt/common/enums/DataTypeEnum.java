d8c49f98077f7d905dfe66ea37a02cb3c5752a38
nianjun <nianjun@bogon>
2016-06-01 17:42:28 +0800

    添加主数据所需枚举

---
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/DataTypeEnum.java
@@ -0,0 +1,47 @@
+package cn.rongcapital.mkt.common.enums;
+
+public enum DataTypeEnum {
+
+    PARTY(0, "主数据"),
+    APP(1, "APP"),
+    POS(2, "POS"),
+    PUBLIC(3, "公众号"),
+    PERSONAL(4, "个人号"),
+    ESHOP(5, "网上商城"),
+    CRM(6, "CRM"),
+    TMAL(7, "天猫"),
+
+    ;
+
+    private DataTypeEnum(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    /**
+     * 数据类型代码
+     */
+    private int code;
+
+    /**
+     * 数据类型描述
+     */
+    private String description;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
