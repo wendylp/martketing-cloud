97258ffab5ca4aca4b9c935b8b307ea95b2cdf95
nianjun <nianjun@bogon>
2016-06-01 18:51:16 +0800

    更新主数据相关代码

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainListServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainListServiceImpl.java
@@ -6,14 +6,15 @@ import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
-import org.apache.ibatis.type.DateTypeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.common.enums.DataTypeEnum;
+import cn.rongcapital.mkt.dao.DataAppDao;
 import cn.rongcapital.mkt.dao.DataPartyDao;
+import cn.rongcapital.mkt.po.DataApp;
 import cn.rongcapital.mkt.po.DataParty;
 import cn.rongcapital.mkt.service.DataGetMainListService;
 import cn.rongcapital.mkt.vo.BaseOutput;
@@ -24,55 +25,91 @@ public class DataGetMainListServiceImpl implements DataGetMainListService {
     @Autowired
     private DataPartyDao dataPartyDao;
 
+    @Autowired
+    private DataAppDao dataAppDao;
+
     @Override
-    public Object getMainList(String method, String userToken, Integer dateType, Integer index,
+    public Object getMainList(String method, String userToken, Integer dataType, Integer index,
                     Integer size, String ver) {
 
         BaseOutput rseult = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
                         ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
+
+        if (dataType == DataTypeEnum.PARTY.getCode()) {
+            assignPartyData(rseult, index, size);
+        } // 获取App数据
+        else if (dataType == DataTypeEnum.APP.getCode()) {
+            assignAppData(rseult, index, size);
+        }
+
 
         rseult.setTotal(rseult.getData().size());
 
         return Response.ok().entity(rseult).build();
     }
 
-    private void assignDataByDataType(int dateType, BaseOutput rseult, int index, int size) {
+    // 这代码写的太2了
+    private void assignPartyData(BaseOutput rseult, int index, int size) {
 
-        if (dateType == DataTypeEnum.PARTY.getCode()) {
-            DataParty paramParty = new DataParty();
-            paramParty.setStartIndex(index);
-            paramParty.setPageSize(size);
+        DataParty paramParty = new DataParty();
+        paramParty.setStartIndex(index);
+        paramParty.setPageSize(size);
+        paramParty.setDeleted(Boolean.FALSE);
 
-            List<DataParty> partyList = dataPartyDao.selectList(paramParty);
-            if (partyList != null && !partyList.isEmpty()) {
-                for (DataParty dataParty : partyList) {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("data_id", dataParty.getId());
-                    map.put("account_name", dataParty.getAccountName());
-                    map.put("name", dataParty.getName());
-                    map.put("gender", dataParty.getGender());
-                    map.put("age", dataParty.getAge());
-                    map.put("home_address", dataParty.getHomeAddress());
-                    map.put("work_address", dataParty.getWorkAddress());
-                    map.put("home_status", dataParty.getHomeStatus());
-                    map.put("work_status", dataParty.getWorkStatus());
-                    map.put("member_level", dataParty.getMemberLevel());
-                    map.put("mobile", dataParty.getMobile());
-                    map.put("email", dataParty.getEmail());
-                    map.put("wechat", dataParty.getWechat());
-                    map.put("qq", dataParty.getQq());
-                    map.put("weibo", dataParty.getWeibo());
-                    map.put("order_count", dataParty.getOrderCount());
-                    map.put("order_amount", dataParty.getOrderAmount());
-                    map.put("cart_item_count", dataParty.getCartItemCount());
-                    map.put("favorite_item_count", dataParty.getFavoriteItemCount());
-                    map.put("salary", dataParty.getSalary());
-                    map.put("offline_activity_attendence",
-                                    dataParty.getOfflineActivityAttendence());
-                    rseult.getData().add(map);
-                }
+        List<DataParty> dataPartyList = dataPartyDao.selectList(paramParty);
+        if (dataPartyList != null && !dataPartyList.isEmpty()) {
+            for (DataParty dataParty : dataPartyList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("data_id", dataParty.getId());
+                map.put("account_name", dataParty.getAccountName());
+                map.put("name", dataParty.getName());
+                map.put("gender", dataParty.getGender());
+                map.put("age", dataParty.getAge());
+                map.put("home_address", dataParty.getHomeAddress());
+                map.put("work_address", dataParty.getWorkAddress());
+                map.put("home_status", dataParty.getHomeStatus());
+                map.put("work_status", dataParty.getWorkStatus());
+                map.put("member_level", dataParty.getMemberLevel());
+                map.put("mobile", dataParty.getMobile());
+                map.put("email", dataParty.getEmail());
+                map.put("wechat", dataParty.getWechat());
+                map.put("qq", dataParty.getQq());
+                map.put("weibo", dataParty.getWeibo());
+                map.put("order_count", dataParty.getOrderCount());
+                map.put("order_amount", dataParty.getOrderAmount());
+                map.put("cart_item_count", dataParty.getCartItemCount());
+                map.put("favorite_item_count", dataParty.getFavoriteItemCount());
+                map.put("salary", dataParty.getSalary());
+                map.put("offline_activity_attendence", dataParty.getOfflineActivityAttendence());
+
+                rseult.getData().add(map);
             }
         }
+    }
 
+    private void assignAppData(BaseOutput rseult, int index, int size) {
+        DataApp paramDataApp = new DataApp();
+        paramDataApp.setStartIndex(index);
+        paramDataApp.setPageSize(size);
+        paramDataApp.setDeleted(Boolean.FALSE);
+
+        List<DataApp> dataAppList = dataAppDao.selectList(paramDataApp);
+        if (dataAppList != null && !dataAppList.isEmpty()) {
+            for (DataApp dataApp : dataAppList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("data_id", dataApp.getId());
+                map.put("account_name", dataApp.getAccountName());
+                map.put("name", dataApp.getName());
+                map.put("delivery_address", dataApp.getDeliveryAddress());
+                map.put("mobile", dataApp.getMobile());
+                map.put("email", dataApp.getEmail());
+                map.put("order_count", dataApp.getOrderCount());
+                map.put("order_amount", dataApp.getOrderAmount());
+                map.put("cart_item_count", dataApp.getCartItemCount());
+                map.put("favorite_item_count", dataApp.getFavoriteItemCount());
+
+                rseult.getData().add(map);
+            }
+        }
     }
 }
