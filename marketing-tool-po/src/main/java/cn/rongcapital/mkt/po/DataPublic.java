ece55807ced16fdff3a456a161294b0687f16f53
nianjun <nianjun@bogon>
2016-06-01 15:01:53 +0800

    提交主数据管理相关代码

---
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/DataPublic.java
@@ -0,0 +1,75 @@
+package cn.rongcapital.mkt.po;
+
+import cn.rongcapital.mkt.po.base.BaseQuery;
+
+public class DataPublic extends BaseQuery {
+    private Integer id;
+
+    private Integer openId;
+
+    private String publicName;
+
+    private String nickName;
+
+    private String iconUrl;
+
+    private Boolean gender;
+
+    private String area;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(Integer openId) {
+        this.openId = openId;
+    }
+
+    public String getPublicName() {
+        return publicName;
+    }
+
+    public void setPublicName(String publicName) {
+        this.publicName = publicName == null ? null : publicName.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl == null ? null : iconUrl.trim();
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+}
--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainListServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainListServiceImpl.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Service;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
-import cn.rongcapital.mkt.dao.PartyDao;
+import cn.rongcapital.mkt.dao.DataPartyDao;
+import cn.rongcapital.mkt.po.DataParty;
 import cn.rongcapital.mkt.po.Party;
 import cn.rongcapital.mkt.service.DataGetMainListService;
 import cn.rongcapital.mkt.vo.BaseOutput;
@@ -20,44 +21,44 @@ import cn.rongcapital.mkt.vo.BaseOutput;
 public class DataGetMainListServiceImpl implements DataGetMainListService {
 
     @Autowired
-    private PartyDao partyDao;
+    private DataPartyDao dataPartyDao;
 
     @Override
     public Object getMainList(String method, String userToken, Integer index, Integer size,
                     String ver) {
-        Party paramParty = new Party();
+        DataParty paramParty = new DataParty();
         paramParty.setStartIndex(index);
         paramParty.setPageSize(size);
 
-        List<Party> partyList = partyDao.selectList(paramParty);
+        List<DataParty> partyList = dataPartyDao.selectList(paramParty);
 
         BaseOutput rseult = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
                         ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
 
         if (partyList != null && !partyList.isEmpty()) {
-            for (Party party : partyList) {
+            for (DataParty dataParty : partyList) {
                 Map<String, Object> map = new HashMap<>();
-                map.put("data_id", party.getId());
-                map.put("account_name", party.getAccountName());
-                map.put("name", party.getName());
-                map.put("gender", party.getGender());
-                map.put("age", party.getAge());
-                map.put("home_address", party.getHomeAddress());
-                map.put("work_address", party.getWorkAddress());
-                map.put("home_status", party.getHomeStatus());
-                map.put("work_status", party.getWorkStatus());
-                map.put("member_level", party.getMemberLevel());
-                map.put("mobile", party.getMobile());
-                map.put("email", party.getEmail());
-                map.put("wechat", party.getWechat());
-                map.put("qq", party.getQq());
-                map.put("weibo", party.getWeibo());
-                map.put("order_count", party.getOrderCount());
-                map.put("order_amount", party.getOrderAmount());
-                map.put("cart_item_count", party.getCartItemCount());
-                map.put("favorite_item_count", party.getFavoriteItemCount());
-                map.put("salary", party.getSalary());
-                map.put("offline_activity_attendence", party.getOfflineActivityAttendence());
+                map.put("data_id", dataParty.getId());
+                map.put("account_name", dataParty.getAccountName());
+                map.put("name", dataParty.getName());
+                map.put("gender", dataParty.getGender());
+                map.put("age", dataParty.getAge());
+                map.put("home_address", dataParty.getHomeAddress());
+                map.put("work_address", dataParty.getWorkAddress());
+                map.put("home_status", dataParty.getHomeStatus());
+                map.put("work_status", dataParty.getWorkStatus());
+                map.put("member_level", dataParty.getMemberLevel());
+                map.put("mobile", dataParty.getMobile());
+                map.put("email", dataParty.getEmail());
+                map.put("wechat", dataParty.getWechat());
+                map.put("qq", dataParty.getQq());
+                map.put("weibo", dataParty.getWeibo());
+                map.put("order_count", dataParty.getOrderCount());
+                map.put("order_amount", dataParty.getOrderAmount());
+                map.put("cart_item_count", dataParty.getCartItemCount());
+                map.put("favorite_item_count", dataParty.getFavoriteItemCount());
+                map.put("salary", dataParty.getSalary());
+                map.put("offline_activity_attendence", dataParty.getOfflineActivityAttendence());
                 rseult.getData().add(map);
             }
         }
