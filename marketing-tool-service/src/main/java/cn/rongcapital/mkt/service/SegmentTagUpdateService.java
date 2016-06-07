4b45232ebbb5efa9b1478dea51043071904caf8c
zhuxuelong <zhuxuelong@ruixuesoft.com>
2016-06-07 14:26:49 +0800

    接口名称：mkt.segment.tag.get 接口名称：mkt.segment.tag.update

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/SegmentTagUpdateService.java
@@ -0,0 +1,22 @@
+/*************************************************
+ * @功能简述: 接口mkt.segment.tag.update的service接口类
+ * @项目名称: marketing cloud
+ * @see: 
+ * @author: 朱学龙
+ * @version: 0.0.1
+ * @date: 2016/6/7
+ * @复审人: 
+ *************************************************/
+package cn.rongcapital.mkt.service;
+
+import javax.ws.rs.core.SecurityContext;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+import cn.rongcapital.mkt.vo.in.SegmentTagUpdateIn;
+
+public interface SegmentTagUpdateService {
+
+	public BaseOutput updateSegmentTag(SegmentTagUpdateIn body,
+			SecurityContext securityContext);
+
+}
