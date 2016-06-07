863de816bf2fe61dfbd66c3b152f4ebbe601db08
kane.sun <taojintianxia@126.com>
2016-06-07 17:08:33 +0800

    看着烦

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/api/MktApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/api/MktApi.java
@@ -116,104 +116,157 @@ import cn.rongcapital.mkt.vo.out.DataGetFilterRecentTaskOut;
 public class MktApi {
 	@Autowired
 	private LoginService loginService;
+	
 	@Autowired
 	private ModifyPasswdService modifyPasswdService;
+	
 	@Autowired
 	private SegmentHeaderCreateService segmentHeaderService;
+	
 	@Autowired
 	private SegmentPublishStatusCountService segmentPublishStatusCountService;
+	
 	@Autowired
 	private SegmentPublishstatusListService segmentPublishstatusListService;
+	
 	@Autowired
 	private DeleteImgTextAssetService deleteImgTextAssetService;
+	
 	@Autowired
 	private GetImgTextAssetService getImgTextAssetService;
+	
 	@Autowired
 	private ImgtextHostService imgtextHostService;
+	
 	@Autowired
 	private WechatTypeCountGetService wechatTypeCountGetService;
+	
 	@Autowired
 	private MigrationFileGeneralInfoService migrationFileGeneralInfoService;
+	
 	@Autowired
 	private MigrationFileTemplateService migrationFileTemplateService;
+	
 	@Autowired
 	private MigrationFileUploadUrlService migrationFileUploadUrlService;
+	
 	@Autowired
 	private DataGetQualityListService dataGetQualityListService;
+	
 	@Autowired
 	private DataGetQualityCountService dataGetQualityCountService;
+	
 	@Autowired
 	private DataGetUnqualifiedCountService dataGetUnqualifiedCountService;
+	
 	@Autowired
 	private DataGetMainCountService dataGetMainCountService;
+	
 	@Autowired
 	private DataGetMainListService dataGetMainListService;
+	
 	@Autowired
 	private DataDeleteMainService dataDeleteMainService;
+	
 	@Autowired
 	private DataGetViewListService dataGetViewListService;
+	
 	@Autowired
 	private DataGetFilterContactwayService dataGetFilterContactwayService;
+	
 	@Autowired
 	private DataGetFilterRecentTaskService dataGetFilterRecentTaskService;
+	
 	@Autowired
 	private SegmentHeaderGetService segmentHeaderGetService;
+	
 	@Autowired
 	private WechatAssetListService wechatAssetListService;
+	
 	@Autowired
 	private WechatAssetListGetService wechatAssetListGetService;
+	
 	@Autowired
 	private UpdateNicknameService updateNicknameService;
+	
 	@Autowired
 	private SaveWechatAssetListService saveWechatAssetListService;
+	
 	@Autowired
 	private SegmentHeaderUpdateService segmentHeaderUpdateService;
+	
 	@Autowired
 	private UploadFileService uploadFileService;
+	
 	@Autowired
 	private CampaignHeaderGetService campaignHeaderGetService;
+	
     @Autowired
     private CampaignBodyGetService campaignBodyGetService;
+    
     @Autowired
     private CampaignBodyCreateService campaignBodyCreateService;
+    
     @Autowired
     private AudienceListService audienceListService;
+    
     @Autowired
     private AudienceListPartyMapService audienceListPartyMapService;
+    
+    @Autowired
     private SegmentBodyUpdateService segmentBodyUpdateService;
+    
 	@Autowired
 	private GetImgtextAssetMenulistService getImgtextAssetMenulistService;
+	
 	@Autowired
 	private TaskListGetService taskListGetService;
+	
 	@Autowired
 	private CampaignDeleteService campaignDeleteService;
+	
 	@Autowired
 	private CampaignSummaryGetService campaignSummaryGetService;
+	
 	@Autowired
 	private CampaignProgressStatusCountService campaignProgressStatusCountService;
+	
 	@Autowired
 	private CampaignProgressStatusListService campaignProgressStatusListService;
+	
 	@Autowired
 	private SegmentTagnameTagListService segmentTagnameTagListService;
+	
 	@Autowired
 	private SegmentTagkeyTagListService segmentTagkeyTagListService;
+	
 	@Autowired
 	private SegmentTagnameTagValueService segmentTagnameTagValueService;
+	
 	@Autowired
 	private TagSystemTagcountService tagSystemTagcountService;
+	
 	@Autowired
 	private SegmentBodyGetService segmentBodyGetService;
+	
 	@Autowired
 	private CustomTagGetService customTagGetService;
+	
 	@Autowired
 	private CustomTagDeleteService customTagDeleteService;
+	
+	@Autowired
 	private MainActionInfoGetService mainActionInfoGetService;
+	
 	@Autowired
 	private SegmentTagGetService segmentTagGetService;
+	
 	@Autowired
 	private SegmentTagUpdateService segmentTagUpdateService;
+	
 	@Autowired
 	private GetImgtextCountService getImgtextCountService;
+	
 	@Autowired
 	private DataMainRadarInfoGetService dataMainRadarInfoGetService;
 
