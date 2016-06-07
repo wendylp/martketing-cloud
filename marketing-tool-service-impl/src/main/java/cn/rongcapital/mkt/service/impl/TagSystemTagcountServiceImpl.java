41eba2fd9859a2f6bcb356ba2db4a02b04e16160
Wey <ziya_0303@aliyun.com>
2016-06-07 11:09:17 +0800

    zhangwei commit

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TagSystemTagcountServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TagSystemTagcountServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.rongcapital.mkt.service.impl;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Service;
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.TagDao;
-import cn.rongcapital.mkt.po.Taggroup;
+import cn.rongcapital.mkt.dao.TaskRunLogDao;
+import cn.rongcapital.mkt.po.TaskRunLog;
 import cn.rongcapital.mkt.service.TagSystemTagcountService;
 import cn.rongcapital.mkt.vo.BaseOutput;
 
@@ -19,17 +22,21 @@ public class TagSystemTagcountServiceImpl implements TagSystemTagcountService {
 	@Autowired
 	TagDao tagDao;
 	
+	@Autowired
+	TaskRunLogDao taskRunLogDao;
+	
 	@Override
 	public BaseOutput getTagcount(String method, String userToken) {
-		int tagCount = tagDao.selectListCount();
-		
-		
+		int tagCount = tagDao.selectAllListCount();
+		List<TaskRunLog> taskRunLogList = taskRunLogDao.selectLastOne();
 		BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
 				   ApiErrorCode.SUCCESS.getMsg(),
 				   ApiConstant.INT_ONE,null);
 		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("tag_group_id", "");
-		map.put("tag_group_name", "");
+		map.put("tag_count", tagCount);
+		if(null != taskRunLogList && taskRunLogList.size() > 0){
+			map.put("sync_time", taskRunLogList.get(0).getEndTime());
+		}
 		result.getData().add(map);
 		return result;
 	}
