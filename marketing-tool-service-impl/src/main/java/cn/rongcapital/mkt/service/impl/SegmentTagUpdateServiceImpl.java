ee166a7d426035e7c4add2b4861f295f7dedc0e0
zhuxuelong <zhuxuelong@ruixuesoft.com>
2016-06-07 14:28:58 +0800

    接口名称：mkt.segment.tag.update

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentTagUpdateServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentTagUpdateServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import javax.ws.rs.core.SecurityContext;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
