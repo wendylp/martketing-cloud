97bd81e9a217eb6c4fbd5372e6aa2dc9696993f4
Wey <ziya_0303@aliyun.com>
2016-06-07 16:26:29 +0800

    zhangwei

--- marketing-tool-dao/src/main/java/cn/rongcapital/mkt/dao/TaskRunLogDao.java
+++ marketing-tool-dao/src/main/java/cn/rongcapital/mkt/dao/TaskRunLogDao.java
@@ -1,47 +1,44 @@
 /*************************************************
- * @功能�?�?: DAO接口�?
+ * @功能简述: DAO接口类
  * @项目名称: marketing cloud
  * @see: 
- * @author: 宋世�?
+ * @author: 宋世涛
  * @version: 0.0.1
  * @date: 2016/5/16
- * @复审�?: 
+ * @复审人: 
 *************************************************/
 
 package cn.rongcapital.mkt.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import cn.rongcapital.mkt.dao.base.BaseDao;
 import cn.rongcapital.mkt.po.TaskRunLog;
 
 public interface TaskRunLogDao extends BaseDao<TaskRunLog>{
 	
-	//自定义扩�?
+	//自定义扩展
 	/**
-	 * 父类方法无法满足�?求时使用,�?在mapper.xml中扩�?
+	 * 父类方法无法满足需求时使用,需在mapper.xml中扩展
 	 * 查询对象list;
-	 * 自定义条件查�?,只要不为NULL与空则为条件,属�?��?�之间and连接
+	 * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
 	 * @param paramMap
 	 * @return list
 	 */
 	//List<T> selectListBycustomMap(Map<String,Object> paramMap);
 	
-	//自定义扩�?
+	//自定义扩展
 	/**
-	 * 父类方法无法满足�?求时使用,�?在mapper.xml中扩�?
+	 * 父类方法无法满足需求时使用,需在mapper.xml中扩展
 	 * 查询对象总数
-	 * 自定义条件查�?,只要不为NULL与空则为条件,属�?��?�之间and连接
+	 * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
 	 * @param paramMap
 	 * @return list
 	 */
 	//List<T> selectListCountBycustomMap(Map<String,Object> paramMap);
-	
-	List<TaskRunLog> selectLastOne(Map<String,Object> paramMap);
 
 	/**
-	 * 查询�?近发生的10个任�?
+	 * 查询最近发生的10个任务
 	 * @param
 	 * @return list
 	 */
