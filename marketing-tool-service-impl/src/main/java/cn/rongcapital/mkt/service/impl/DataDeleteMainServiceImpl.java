dfea1f619a5e2f982bafa291d339789ff4ad94d7
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-13 18:08:00 +0800

    注释掉一个将来可能会加回来的方法

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataDeleteMainServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataDeleteMainServiceImpl.java
@@ -1,18 +1,10 @@
 package cn.rongcapital.mkt.service.impl;
 
-import java.util.Date;
-
-import javax.ws.rs.core.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.rongcapital.mkt.common.constant.ApiConstant;
-import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.DataPartyDao;
-import cn.rongcapital.mkt.po.DataParty;
 import cn.rongcapital.mkt.service.DataDeleteMainService;
-import cn.rongcapital.mkt.vo.BaseOutput;
 
 @Service
 public class DataDeleteMainServiceImpl implements DataDeleteMainService {
@@ -23,22 +15,23 @@ public class DataDeleteMainServiceImpl implements DataDeleteMainService {
     @Override
     public Object deleteMain(String method, String userToken, String ver, Integer dataId) {
 
-        BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
-                        ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
+//        BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
+//                        ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
+//
+//        DataParty paramDataParty = new DataParty();
+//        paramDataParty.setId(dataId);
+//        paramDataParty.setDeleted(Boolean.TRUE);
+//        paramDataParty.setDeleteTime(new Date());
+//
+//        int rowEffected = dataPartyDao.logicDeleteById(paramDataParty);
+//        if (rowEffected == 0) {
+//            result.setMsg("不存在id为" + dataId + "的数据");
+//        }
+//
+//        result.setTotal(result.getData().size());
 
-        DataParty paramDataParty = new DataParty();
-        paramDataParty.setId(dataId);
-        paramDataParty.setDeleted(Boolean.TRUE);
-        paramDataParty.setDeleteTime(new Date());
-
-        int rowEffected = dataPartyDao.logicDeleteById(paramDataParty);
-        if (rowEffected == 0) {
-            result.setMsg("不存在id为" + dataId + "的数据");
-        }
-
-        result.setTotal(result.getData().size());
-
-        return Response.ok().entity(result).build();
+//        return Response.ok().entity(result).build();
+        return null;
     }
 
 }
