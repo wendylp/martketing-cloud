0065646a16df1131114c8bc7429fbffe20e4dd5f
jason <12481228@qq.com>
2016-06-14 19:12:48 +0800

    提交定时任务demo

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/JobDemo.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/JobDemo.java
@@ -1,21 +1,21 @@
-package cn.rongcapital.mkt.job;
-
-import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
-import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
-
-public class JobDemo extends AbstractSimpleElasticJob {
-
-	@Override
-	public void process(JobExecutionMultipleShardingContext context) {
-		
-		context.createJobExecutionSingleShardingContext(1);
-		System.out.println("job starts running....");
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("job finished");
-	}
-
-}
+//package cn.rongcapital.mkt.job;
+//
+//import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+//import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+//
+//public class JobDemo extends AbstractSimpleElasticJob {
+//
+//	@Override
+//	public void process(JobExecutionMultipleShardingContext context) {
+//		
+//		context.createJobExecutionSingleShardingContext(1);
+//		System.out.println("job starts running....");
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("job finished");
+//	}
+//
+//}
