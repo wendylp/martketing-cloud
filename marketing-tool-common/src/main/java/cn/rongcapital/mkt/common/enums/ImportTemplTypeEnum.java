ac15024993bbf64e0933fcdd98871fd89776cf7e
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-16 13:32:15 +0800

    更新相关enum代码

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/ImportTemplTypeEnum.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/ImportTemplTypeEnum.java
@@ -1,14 +1,11 @@
 package cn.rongcapital.mkt.common.enums;
 
 public enum ImportTemplTypeEnum {
-    PARTY("data_party_rows", "主数据"), 
-    POPULATION("data_population_rows", "人口属性"), 
-    CUSTOMER_TAGS("data_customer_tags_rows", "客户标签"), 
-    ARCH_POINT("data_arch_point_rows", "埋点统计"), 
-    MEMBER("data_member_rows", "会员卡记录"), 
-    LOGIN("data_login_rows", "登录行为"), 
-    PAYMENT("data_payment_rows", "支付记录"), 
-    SHOPPING("data_shopping_rows", "购物记录"),
+    PARTY("data_party_rows", "主数据"), POPULATION("data_population_rows", "人口属性"), CUSTOMER_TAGS(
+                    "data_customer_tags_rows",
+                    "客户标签"), ARCH_POINT("data_arch_point_rows", "埋点统计"), MEMBER("data_member_rows",
+                                    "会员卡记录"), LOGIN("data_login_rows", "登录行为"), PAYMENT("data_payment_rows",
+                                                    "支付记录"), SHOPPING("data_shopping_rows", "购物记录"),
 
     ;
     private ImportTemplTypeEnum(String countName, String name) {
@@ -35,15 +32,26 @@ public enum ImportTemplTypeEnum {
     public void setCountName(String countName) {
         this.countName = countName;
     }
-    
-    public static String getNameByCountName(String countName){
+
+    public static String getNameByCountName(String countName) {
         ImportTemplTypeEnum[] importTemplTypeEnumArray = ImportTemplTypeEnum.values();
-        for(ImportTemplTypeEnum importTemplTypeEnum : importTemplTypeEnumArray){
-            if(importTemplTypeEnum.getCountName().equalsIgnoreCase(countName)){
+        for (ImportTemplTypeEnum importTemplTypeEnum : importTemplTypeEnumArray) {
+            if (importTemplTypeEnum.getCountName().equalsIgnoreCase(countName)) {
                 return importTemplTypeEnum.getName();
             }
         }
-        
+
+        return "";
+    }
+
+    public static String getCountNameByName(String name) {
+        ImportTemplTypeEnum[] importTemplTypeEnumArray = ImportTemplTypeEnum.values();
+        for (ImportTemplTypeEnum importTemplTypeEnum : importTemplTypeEnumArray) {
+            if (importTemplTypeEnum.getName().equalsIgnoreCase(name)) {
+                return importTemplTypeEnum.getCountName();
+            }
+        }
+
         return "";
     }
 
