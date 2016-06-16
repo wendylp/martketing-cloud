bd7e0fc73fa70ed2a7e60c94cd490d7d3f97f603
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-16 13:43:51 +0800

    加业务逻辑....

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainCountServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainCountServiceImpl.java
@@ -1,5 +1,8 @@
 package cn.rongcapital.mkt.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
@@ -9,8 +12,10 @@ import org.springframework.stereotype.Service;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
+import cn.rongcapital.mkt.common.enums.ImportTemplTypeEnum;
 import cn.rongcapital.mkt.dao.DataPartyDao;
 import cn.rongcapital.mkt.dao.ImportTemplateDao;
+import cn.rongcapital.mkt.po.ImportTemplate;
 import cn.rongcapital.mkt.service.DataGetMainCountService;
 import cn.rongcapital.mkt.vo.out.DataGetMainCountOut;
 
@@ -19,7 +24,7 @@ public class DataGetMainCountServiceImpl implements DataGetMainCountService {
 
     @Autowired
     private DataPartyDao dataPartyDao;
-    
+
     @Autowired
     private ImportTemplateDao importTemplateDao;
 
@@ -29,10 +34,25 @@ public class DataGetMainCountServiceImpl implements DataGetMainCountService {
         DataGetMainCountOut result = new DataGetMainCountOut(ApiErrorCode.SUCCESS.getCode(),
                         ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
 
-        Map<String, Object> resultMap = dataPartyDao.selectMainCount();
-        Map<String, Integer> mdTypesMap = importTemplateDao.selectMainCountPair();
-        result.getData().add(resultMap);
-        result.getMdTypesPair().putAll(mdTypesMap);
+        Map<String, Object> countRowsMap = dataPartyDao.selectMainCount();
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        List<ImportTemplate> importTemplateList = importTemplateDao.selectTemplTypePairs();
+        for (Map.Entry<String, Object> entry : countRowsMap.entrySet()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("count_rows", entry.getValue());
+            String tagName = ImportTemplTypeEnum.getNameByCountName(entry.getKey());
+            map.put("tag_name", tagName);
+            for (ImportTemplate importTemplate : importTemplateList) {
+                if (importTemplate.getTemplName().equals(tagName)) {
+                    map.put("md_type", importTemplate.getTemplType());
+                    break;
+                }
+            }
+
+            resultList.add(map);
+
+        }
+        result.getData().addAll(resultList);
         result.setTotal(result.getData().size());
 
         return Response.ok().entity(result).build();
--- marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/out/DataGetMainCountOut.java
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/out/DataGetMainCountOut.java
@@ -1,33 +1,16 @@
 package cn.rongcapital.mkt.vo.out;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.codehaus.jackson.annotate.JsonProperty;
 
 import cn.rongcapital.mkt.vo.BaseOutput;
 
 public class DataGetMainCountOut extends BaseOutput {
 
     public DataGetMainCountOut() {
-        mdTypesPair = new HashMap<>();
     }
 
     public DataGetMainCountOut(int code, String msg, int total, List<Object> data) {
         super(code, msg, total, data);
-        mdTypesPair = new HashMap<>();
-    }
-
-    @JsonProperty("md_types")
-    private Map<String, Integer> mdTypesPair;
-
-    public Map<String, Integer> getMdTypesPair() {
-        return mdTypesPair;
-    }
-
-    public void setMdTypesPair(Map<String, Integer> mdTypesPair) {
-        this.mdTypesPair = mdTypesPair;
     }
 
 }
