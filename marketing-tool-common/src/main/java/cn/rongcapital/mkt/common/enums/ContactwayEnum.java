d7629342b0b44c302e49d6b1e2baeaa709756a53
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-20 13:13:13 +0800

    更新下ContactWay的枚举

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/ContactwayEnum.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/ContactwayEnum.java
@@ -2,18 +2,19 @@ package cn.rongcapital.mkt.common.enums;
 
 public enum ContactwayEnum {
 
-    MOBILE(1, "手机"), 
-    EMAIL(2, "电子邮件"), 
-    WECHAT(3, "微信号"),
+    MOBILE(1, "mobile", "手机"), EMAIL(2, "email", "电子邮件"), WECHAT(3, "wechat", "微信号"),
 
     ;
 
-    private ContactwayEnum(int id, String description) {
+    private ContactwayEnum(int id, String columnName, String description) {
+        this.columnName = columnName;
         this.id = id;
         this.description = description;
     }
 
     private int id;
+
+    private String columnName;
 
     private String description;
 
@@ -25,12 +26,31 @@ public enum ContactwayEnum {
         this.id = id;
     }
 
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public static String getColumnNameById(int id) {
+        ContactwayEnum[] contactwayEnums = ContactwayEnum.values();
+        for (ContactwayEnum contactwayEnum : contactwayEnums) {
+            if (contactwayEnum.getId() == id) {
+                return contactwayEnum.getColumnName();
+            }
+        }
+
+        return null;
     }
 
 }
