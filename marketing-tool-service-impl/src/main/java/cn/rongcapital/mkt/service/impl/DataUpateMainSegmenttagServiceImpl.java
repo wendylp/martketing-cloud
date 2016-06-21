b55e407e016dcf87f2dbf29c4884ba2f376932a2
baiyunfeng <baiyunfeng@oneapm.com>
2016-06-21 20:34:08 +0800

    modify bug

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataUpateMainSegmenttagServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataUpateMainSegmenttagServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import cn.rongcapital.mkt.po.CustomTag;
 import cn.rongcapital.mkt.po.CustomTagMap;
 import cn.rongcapital.mkt.service.DataUpateMainSegmenttagService;
 
+@Service
 public class DataUpateMainSegmenttagServiceImpl implements DataUpateMainSegmenttagService {
 
     @Autowired
