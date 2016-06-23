5c4291de94d59d9b4384a5d852d9525574e18030
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-23 16:28:21 +0800

    添加task run log的查询功能

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/TaskGetListService.java
@@ -0,0 +1,15 @@
+package cn.rongcapital.mkt.service;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+
+public interface TaskGetListService {
+
+    /**
+     * mkt.task.list.get
+     * 
+     * @功能简述 : 获取后台任务列表
+     * @author nianjun
+     * @return
+     */
+    public BaseOutput getTaskList();
+}
