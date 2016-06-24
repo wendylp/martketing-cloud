2d1bf7acf539656adbaa91c66985b998efcaf70f
xu kun <xukunlucky@hotmail.com>
2016-06-24 14:20:01 +0800

    add interface

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentFilterGetServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentFilterGetServiceImpl.java
@@ -10,6 +10,7 @@ import heracles.data.common.annotation.ReadWrite;
 import heracles.data.common.util.ReadWriteType;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,6 +34,7 @@ import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.CustomTagMapDao;
 import cn.rongcapital.mkt.po.CustomTagWithName;
 import cn.rongcapital.mkt.po.mongodb.DataParty;
+import cn.rongcapital.mkt.po.mongodb.Tag;
 import cn.rongcapital.mkt.service.SegmentFilterGetService;
 import cn.rongcapital.mkt.vo.BaseOutput;
 import cn.rongcapital.mkt.vo.in.SegmentFilterCondition;
@@ -51,48 +53,220 @@ public class SegmentFilterGetServiceImpl implements SegmentFilterGetService {
     @Override
     public BaseOutput getSegmentFilterCount(SegmentFilterCountIn body, SecurityContext securityContext) {
 
-        
+               
         List<SegmentFilterCondition> conditions=body.getConditions();
         
-        // 拼mongodb 查询条件
-        Query query = new Query();
-        Criteria criteria = new Criteria();
-        Criteria[] criterialist = new Criteria[conditions.size()];
-
-        for (int i = 0; i < conditions.size(); i++) {
+        int count=conditions.size();
+        
+        BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
+                        ApiErrorCode.SUCCESS.getMsg(),
+                        ApiConstant.INT_ZERO,null);
+        
+        result.setTotal(count);
+        
+        
+        for(int i=0;i<count;i++){
             
-            Criteria criteriaand = new Criteria();
-            //根据getExclude()拼条件
+            Map<String,Object> map = new HashMap<String,Object>();
             
-            criteriaand.andOperator(Criteria.where("tagList.tagId")
-                            .is(new String(conditions.get(i).getTag_id())).and("tagId")
-                            .gte(conditions.get(i).getTag_id()));
-            criterialist[i] = criteriaand;
+            map.put("tag_id",conditions.get(i).getTag_id());
+            map.put("tag_name",conditions.get(i).getTag_name());
+            
+            String tag_count=getPeopleCount(i,conditions);
+            
+            map.put("tag_count",tag_count);
+                        
+            result.getData().add(map);            
+            
         }
-
-        criteria.orOperator(criterialist);
-        query.addCriteria(criteria);
-        mongoTemplate.find(query, DataParty.class);
-
-
-
-        BaseOutput baseOutput = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
-                        ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
-        // List<CustomTagWithName> data = customTagMapDao.getTagUseHeadId(Long
-        // .valueOf(segmentHeadId));
-        // if (data != null) {
-        // List<Object> result = new ArrayList<Object>();
-        // SegmentTagGetOut out = null;
-        // for (CustomTagWithName tags : data) {
-        // out = new SegmentTagGetOut();
-        // out.setTagId(tags.getId());
-        // out.setTagName(tags.getName());
-        // result.add(out);
-        // }
-        // baseOutput.setData(result);
-        // baseOutput.setTotal(result.size());
-        // }
-        return baseOutput;
+        
+      return result;
+        
+//        
+//        
+//        
+//        if(count==1){
+//            
+//            map.put("tag_id",conditions.get(0).getTag_id());
+//            map.put("tag_name",conditions.get(0).getTag_name());
+//            String tag_count=getPeopleCount(criterialist);
+//            map.put("tag_count",tag_count);
+//            
+//            result.getData().add(map);
+//        }
+//                
+//        
+//        if(count==2){
+//            
+//            map.put("tag_id",conditions.get(0).getTag_id());
+//            map.put("tag_name",conditions.get(0).getTag_name());
+//            String tag_count=getPeopleCount(criterialist);
+//            map.put("tag_count",tag_count);
+//            
+//            result.getData().add(map);
+//        }
+//        
+//        
+//        
+//        
+//        
+//        
+//        // 拼mongodb 查询条件
+//        Query query = new Query();
+//        Criteria criteria = new Criteria();
+//        Criteria[] criterialist = new Criteria[conditions.size()];
+//        int count=conditions.size();
+//        logger.debug("conditions count="+count);
+//        
+//        List<DataParty> restList=new ArrayList<DataParty>(count);
+//        
+//        
+//        if(count==1){
+//            
+//            
+//            for (int i = 0; i < count; i++) {
+//                
+//                Criteria criteriaAnd = new Criteria();
+//                //根据getExclude()拼条件
+//                
+//                if(conditions.get(i).getExclude().equals("0")){
+//                    
+//                    criteriaAnd.andOperator(Criteria.where("tagList.tagId").is(new String(conditions.get(i).getTag_id())));
+//                                    
+//                }else{
+//                    
+//                    criteriaAnd.andOperator(Criteria.where("tagList.tagId").ne(new String(conditions.get(i).getTag_id())));
+//                }
+//                
+//                criterialist[i] = criteriaAnd;
+//            }
+//
+//            criteria.orOperator(criterialist);
+//            query.addCriteria(criteria);
+//            restList =mongoTemplate.find(query, DataParty.class);
+//                    
+//            
+//        }else if(count==2){
+//            
+//        
+//            
+//        }else if(count==3){
+//            
+//        
+//            
+//            
+//        }else{
+//            
+//            logger.debug("conditions count 非法");
+//        }
+//        
+//
+//        for (int i = 0; i < count; i++) {
+//            
+//            Criteria criteriaAnd = new Criteria();
+//            //根据getExclude()拼条件
+//            
+//            if(conditions.get(i).getExclude().equals("0")){
+//                
+//                criteriaAnd.andOperator(Criteria.where("tagList.tagId").is(new String(conditions.get(i).getTag_id())));
+//                                
+//            }else{
+//                
+//                criteriaAnd.andOperator(Criteria.where("tagList.tagId").ne(new String(conditions.get(i).getTag_id())));
+//            }
+//            
+//            criterialist[i] = criteriaAnd;
+//        }
+//
+//        criteria.orOperator(criterialist);
+//        query.addCriteria(criteria);
+//        List<DataParty> restList =mongoTemplate.find(query, DataParty.class);
+        
+//        Map<String,Object> map = new HashMap<String,Object>();
+//        
+//        map.put("tag_id",tagid);
+//        
+//        int count=restList.size();          
+//        map.put("tag_count", count);            
+//        
+//        List<Tag> tagList = restList.get(0).getTagList();
+//        for(Tag tag : tagList){
+//            
+//            if(tagid.equals(tag.getTagId())){
+//                map.put("tag_name", tag.getTagName());
+//                break;
+//            }
+//        }
+//        
+//        result.getData().add(map);
+        
+//    }
+    
+    
+    
     }
+    
+    
+//    private Criteria[] buildConditions(List<SegmentFilterCondition> conditions){
+//        
+//        Criteria[] criterialist = new Criteria[conditions.size()];
+//        int count=conditions.size();
+//        
+//        for (int i = 0; i < count; i++) {
+//            
+//            Criteria criteriaAnd = new Criteria();
+//            
+//            //根据getExclude()拼条件            
+//            if(conditions.get(i).getExclude().equals("0")){
+//                
+//                criteriaAnd.andOperator(Criteria.where("tagList.tagId").is(new String(conditions.get(i).getTag_id())));
+//                                
+//            }else{
+//                
+//                criteriaAnd.andOperator(Criteria.where("tagList.tagId").ne(new String(conditions.get(i).getTag_id())));
+//            }
+//            
+//            criterialist[i] = criteriaAnd;
+//        }
+//        
+//        return criterialist;
+//              
+//        
+//    }
+    
+    
+    private String getPeopleCount(int idx,List<SegmentFilterCondition> conditions){
+        
+        Query query = new Query();
+        Criteria criteria = new Criteria(); 
+        
+        Criteria[] criterialist = new Criteria[idx+1];
+        
+        
+        for(int i=0;i<idx+1;i++){
+            
+            Criteria criteriaAnd = new Criteria();
+            
+            //根据getExclude()拼条件            
+            if(conditions.get(i).getExclude().equals("0")){
+                
+                criteriaAnd.andOperator(Criteria.where("tagList.tagId").is(new String(conditions.get(i).getTag_id())));
+                                
+            }else{
+                
+                criteriaAnd.andOperator(Criteria.where("tagList.tagId").ne(new String(conditions.get(i).getTag_id())));
+            }
+            
+            criterialist[i] = criteriaAnd;
+                        
+        }
+        
+        criteria.andOperator(criterialist);
+        query.addCriteria(criteria);
+        long tag_count=mongoTemplate.count(query, DataParty.class);
+        return  Long.toString(tag_count);     
+        
+    }
+
 
 }
