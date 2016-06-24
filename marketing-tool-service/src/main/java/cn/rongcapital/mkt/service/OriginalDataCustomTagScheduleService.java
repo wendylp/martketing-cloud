c04b5a74f7a26c5132297d29c2f04571168728a1
bianyulong <bianyulong@rongcapital.cn>
2016-06-24 16:18:51 +0800

    添加客户标签数据清理

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/OriginalDataCustomTagScheduleService.java
@@ -0,0 +1,10 @@
+package cn.rongcapital.mkt.service;
+
+import cn.rongcapital.mkt.service.base.OriginalDataScheduleService;
+
+/**
+ * Created by bianyulong on 16/6/24.
+ */
+public interface OriginalDataCustomTagScheduleService extends OriginalDataScheduleService{
+
+}
