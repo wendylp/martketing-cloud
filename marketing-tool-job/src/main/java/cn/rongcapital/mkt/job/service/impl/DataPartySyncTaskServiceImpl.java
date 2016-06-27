81fae79bc65d5b3a949e1dd2e4e4c19ff7064ab5
xu kun <xukunlucky@hotmail.com>
2016-06-27 18:56:32 +0800

    modify DataPartySyncTaskServiceImpl

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/DataPartySyncTaskServiceImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/DataPartySyncTaskServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.rongcapital.mkt.job.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -25,7 +26,14 @@ import cn.rongcapital.mkt.dao.TenementDao;
 import cn.rongcapital.mkt.job.service.base.TaskService;
 import cn.rongcapital.mkt.mongodb.DataPartyRepository;
 import cn.rongcapital.mkt.po.Tag;
-import cn.rongcapital.mkt.po.mongodb.DataParty;
+import cn.rongcapital.mkt.po.DataArchPoint;
+import cn.rongcapital.mkt.po.DataCustomerTags;
+import cn.rongcapital.mkt.po.DataLogin;
+import cn.rongcapital.mkt.po.DataMember;
+import cn.rongcapital.mkt.po.DataParty;
+import cn.rongcapital.mkt.po.DataPayment;
+import cn.rongcapital.mkt.po.DataPopulation;
+import cn.rongcapital.mkt.po.DataShopping;
 
 @Service
 public class DataPartySyncTaskServiceImpl implements TaskService {
@@ -67,27 +75,102 @@ public class DataPartySyncTaskServiceImpl implements TaskService {
 	public void task(Integer taskId) {
 	    		
 	    //1.从mysql data_party表中读取所有mid,keyid
-	    //Tag tag=new Tag();
-	    //tag.setStatus(0);
-	    //List<String> tags=tagDao.selectList(tag); 
+	    DataParty dataParty=new DataParty();	    
+	    //dataParty.setStatus(0);	    
+	    
+	    //分批读出
+	    List<DataParty> datapartyList=dataPartyDao.selectList(dataParty); 
 	    
 	    //2.根据keyid在data_xxx表中查出所有字段插入mongodb data_party表中
-	    
-	    	    
-	    
-	    logger.info(taskId.toString());
-		List<DataParty> dpList = null;
-		dpList = dataPartyRepository.findAll();
-		for(DataParty dp:dpList) {
-			System.out.println(JSON.toJSONString(dp));
-		}
-		dpList = dataPartyRepository.findByMid("101");
-		for(DataParty dp:dpList) {
-			System.out.println(JSON.toJSONString(dp));
-		}
-		dpList = mongoTemplate.find(new Query(Criteria.where("mid").is("101")),DataParty.class);
-		for(DataParty dp:dpList) {
-			System.out.println(JSON.toJSONString(dp));
-		}
+	    for(DataParty data_party : datapartyList){
+	        
+	        Integer data_type=data_party.getMdType();
+	        String keyid=data_party.getMappingKeyid();
+	        
+	        add2Mongo(data_type,keyid);
+	        
+	    }
+
 	}
+	
+	private void add2Mongo(int data_type,String keyid){
+	    
+	    //data_entity
+        List<Integer> idList=new ArrayList<Integer>();
+        idList.add(Integer.parseInt(keyid));
+	    
+	    //批量插入
+	    if(data_type==1){
+	        
+	        List<DataPopulation> dataList=dataPopulationDao.selectListByIdList(idList);
+	        
+	        //insert into mongodb
+	        for(DataPopulation dataObj : dataList){
+	            ;
+	            //mongoTemplate.createCollection(dataObj);
+	        }
+	        	        
+	        
+	    }else if(data_type==2){
+	        
+	        List<DataCustomerTags> dataList=dataCustomerTagsDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataCustomerTags dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+	        
+	    }else if(data_type==3){
+	        
+	        List<DataArchPoint> dataList=dataArchPointDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataArchPoint dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+        }else if(data_type==4){
+            
+            List<DataMember> dataList=dataMemberDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataMember dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+        }else if(data_type==5){
+            
+            List<DataLogin> dataList=dataLoginDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataLogin dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+        }else if(data_type==6){
+            
+            List<DataPayment> dataList=dataPaymentDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataPayment dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+        }else if(data_type==7){
+            
+            List<DataShopping> dataList=dataShoppingDao.selectListByIdList(idList);
+            
+            //insert into mongodb
+            for(DataShopping dataObj : dataList){
+                ;
+                //mongoTemplate.createCollection(dataObj);
+            }
+        }else{
+            ;
+        }
+	        
+	    
+	}
+	
 }
