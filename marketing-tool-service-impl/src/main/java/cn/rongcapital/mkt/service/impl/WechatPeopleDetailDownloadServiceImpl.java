c2dd6e4e8acf4a35255c5d2442ff7e9bdf67c92b
Sun Nianjun <nianjunsun@rongcapital.cn>
2016-06-27 14:59:38 +0800

    更新fileutil相关代码

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WechatPeopleDetailDownloadServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WechatPeopleDetailDownloadServiceImpl.java
@@ -1,19 +1,22 @@
 package cn.rongcapital.mkt.service.impl;
 
-import cn.rongcapital.mkt.common.constant.ApiConstant;
-import cn.rongcapital.mkt.common.constant.ApiErrorCode;
-import cn.rongcapital.mkt.dao.WechatAssetGroupDao;
-import cn.rongcapital.mkt.dao.WechatMemberDao;
-import cn.rongcapital.mkt.service.WechatPeopleDetailDownloadService;
-import cn.rongcapital.mkt.vo.BaseOutput;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.ws.rs.core.Response;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.common.constant.ApiErrorCode;
+import cn.rongcapital.mkt.common.util.FileUtil;
+import cn.rongcapital.mkt.dao.WechatAssetGroupDao;
+import cn.rongcapital.mkt.dao.WechatMemberDao;
+import cn.rongcapital.mkt.service.WechatPeopleDetailDownloadService;
+import cn.rongcapital.mkt.vo.BaseOutput;
 
 /**
  * Created by Yunfeng on 2016-6-24.
