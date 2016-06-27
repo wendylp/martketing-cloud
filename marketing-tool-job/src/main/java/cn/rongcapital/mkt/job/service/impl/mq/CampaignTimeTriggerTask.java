30aabfce225f5abdd779500106962b2f08b6b732
jason <12481228@qq.com>
2016-06-27 18:31:19 +0800

    手动开启活动接口逻辑完善

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/mq/CampaignTimeTriggerTask.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/mq/CampaignTimeTriggerTask.java
@@ -5,8 +5,11 @@ import javax.jms.MessageConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.dao.CampaignHeadDao;
 import cn.rongcapital.mkt.dao.TaskScheduleDao;
 import cn.rongcapital.mkt.job.service.base.TaskService;
+import cn.rongcapital.mkt.po.CampaignHead;
 import cn.rongcapital.mkt.po.TaskSchedule;
 
 @Service
@@ -16,12 +19,19 @@ public class CampaignTimeTriggerTask extends BaseMQService implements TaskServic
 	
     @Autowired
     private TaskScheduleDao taskScheduleDao;
-    
     MessageConsumer consumer = null;
-    
+	@Autowired
+	CampaignHeadDao campaignHeadDao;
+	
 	@Override
 	public void task(TaskSchedule taskSchedule) {
 		Integer campaignHeadId = taskSchedule.getCampaignHeadId();
+		//设置活动状态为:活动中
+		CampaignHead t = new CampaignHead();
+		t.setId(campaignHeadId);
+		t.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
+		t.setPublishStatus(ApiConstant.CAMPAIGN_PUBLISH_STATUS_IN_PROGRESS);
+		campaignHeadDao.updateById(t);
 		//激活该活动对应的全部任务
 		taskScheduleDao.activateTaskByCampaignHeadId(campaignHeadId);
 	}
@@ -30,6 +40,12 @@ public class CampaignTimeTriggerTask extends BaseMQService implements TaskServic
 		Integer campaignHeadId = taskSchedule.getCampaignHeadId();
 		//停止该活动对应的全部任务
 		taskScheduleDao.deActivateTaskByCampaignHeadId(campaignHeadId);
+		//设置活动状态为:已结束
+		CampaignHead t = new CampaignHead();
+		t.setId(campaignHeadId);
+		t.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
+		t.setPublishStatus(ApiConstant.CAMPAIGN_PUBLISH_STATUS_FINISH);
+		campaignHeadDao.updateById(t);
 	}
 
 	@Override
--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignManualStartServiceImple.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignManualStartServiceImple.java
@@ -1,13 +1,19 @@
 package cn.rongcapital.mkt.service.impl;
 
+import java.util.List;
+
 import javax.ws.rs.core.SecurityContext;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.CampaignHeadDao;
+import cn.rongcapital.mkt.dao.TaskScheduleDao;
 import cn.rongcapital.mkt.job.service.base.TaskManager;
 import cn.rongcapital.mkt.po.CampaignHead;
 import cn.rongcapital.mkt.service.CampaignManualStartService;
@@ -21,17 +27,57 @@ public class CampaignManualStartServiceImple implements CampaignManualStartServi
 	CampaignHeadDao campaignHeadDao;
 	@Autowired
 	TaskManager taskManager;
+	@Autowired
+	private TaskScheduleDao taskScheduleDao;
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public CampaignManualStartOut campaignManualStart(CampaignManualStartIn body, SecurityContext securityContext) {
+		CampaignManualStartOut ur = checkPublishStatus(body.getCampaignHeadId());
+		if(null != ur) {
+			return ur;
+		}		
+		//设置活动状态为:活动中
 		CampaignHead t = new CampaignHead();
 		t.setId(body.getCampaignHeadId());
 		t.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
-		t.setPublishStatus(ApiConstant.SEGMENT_PUBLISH_STATUS_IN_CAMPAIGN);
+		t.setPublishStatus(ApiConstant.CAMPAIGN_PUBLISH_STATUS_IN_PROGRESS);
 		campaignHeadDao.updateById(t);
-		taskManager.manualInitTask();//执行扫描任务表
+		//激活该活动对应的全部任务
+	    taskScheduleDao.activateTaskByCampaignHeadId(body.getCampaignHeadId());
 		CampaignManualStartOut out = new CampaignManualStartOut(ApiConstant.INT_ZERO,ApiErrorCode.SUCCESS.getMsg(),ApiConstant.INT_ZERO,null);
 		return out;
 	}
 
+	private CampaignManualStartOut checkPublishStatus(int id) {
+		 CampaignManualStartOut ur = null;
+		 CampaignHead t = new CampaignHead(); 
+		 t.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
+		 t.setId(id);
+		 List<CampaignHead> segList = campaignHeadDao.selectList(t);
+		 if(CollectionUtils.isNotEmpty(segList)) {
+			 CampaignHead ch = segList.get(0);
+			if(ch.getPublishStatus() == ApiConstant.CAMPAIGN_PUBLISH_STATUS_NOT_PUBLISH) {
+			  ur = new CampaignManualStartOut(ApiErrorCode.BIZ_ERROR_CANPAIGN_NOT_PUBLISH.getCode(),
+									ApiErrorCode.BIZ_ERROR_CANPAIGN_NOT_PUBLISH.getMsg(),
+									ApiConstant.INT_ZERO,null);
+			}
+			if(ch.getPublishStatus() == ApiConstant.CAMPAIGN_PUBLISH_STATUS_IN_PROGRESS) {
+				ur = new CampaignManualStartOut(ApiErrorCode.BIZ_ERROR_CANPAIGN_IN_PROGRESS.getCode(),
+									ApiErrorCode.BIZ_ERROR_CANPAIGN_IN_PROGRESS.getMsg(),
+									ApiConstant.INT_ZERO,null);
+			}
+			if(ch.getPublishStatus() == ApiConstant.CAMPAIGN_PUBLISH_STATUS_FINISH) {
+				ur = new CampaignManualStartOut(ApiErrorCode.BIZ_ERROR_CANPAIGN_FINISH.getCode(),
+									ApiErrorCode.BIZ_ERROR_CANPAIGN_FINISH.getMsg(),
+									ApiConstant.INT_ZERO,null);
+			}
+		 } else {
+			ur = new CampaignManualStartOut(ApiErrorCode.DB_ERROR_TABLE_DATA_NOT_EXIST.getCode(),
+								ApiErrorCode.DB_ERROR_TABLE_DATA_NOT_EXIST.getMsg(),
+								ApiConstant.INT_ZERO,null);
+		 }
+		 return ur;
+	 }
+	
 }
