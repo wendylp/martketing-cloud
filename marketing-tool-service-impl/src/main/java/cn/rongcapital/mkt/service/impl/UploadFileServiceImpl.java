1e9f7b4d8f61df94613a71078ac531d7810fd0df
baiyunfeng <baiyunfeng@oneapm.com>
2016-06-27 14:53:52 +0800

    modify bug

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/UploadFileServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/UploadFileServiceImpl.java
@@ -53,7 +53,7 @@ public class UploadFileServiceImpl implements UploadFileService{
         }
         String fileName = "";
         Map<String,List<InputPart>> uploadForm = fileInput.getFormDataMap();
-        List<InputPart> inputParts = uploadForm.get("uploadFile");
+        List<InputPart> inputParts = uploadForm.get("uploadedFile");
         for(InputPart inputPart : inputParts){
             try {
                 MultivaluedMap<String,String> header = inputPart.getHeaders();
