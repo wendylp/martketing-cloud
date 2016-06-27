31535bda3862a2a445ffaec03e084bf17247a82b
nianjun.sun <sunnianjun@rongcapital.cn>
2016-06-27 10:42:50 +0800

    按照反射的形式更改了下代码

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataDownloadQualityLogServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataDownloadQualityLogServiceImpl.java
@@ -1,9 +1,12 @@
 package cn.rongcapital.mkt.service.impl;
 
 import java.io.File;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import cn.rongcapital.mkt.common.enums.FileNameEnum;
+import cn.rongcapital.mkt.common.util.FileUtil;
 import cn.rongcapital.mkt.dao.ImportDataModifyLogDao;
 import cn.rongcapital.mkt.po.ImportDataModifyLog;
 import cn.rongcapital.mkt.service.DataDownloadQualityLogService;
@@ -12,13 +15,18 @@ public class DataDownloadQualityLogServiceImpl implements DataDownloadQualityLog
 
     @Autowired
     private ImportDataModifyLogDao importDataModifyLogDao;
-    
+
     @Override
     public File downloadQualityLog(Long importDataId) {
-        
-        ImportDataModifyLog importDataModifyLog = new ImportDataModifyLog();
-        importDataModifyLog.setImportDataId(importDataId);
-        
+
+        ImportDataModifyLog paramImportDataModifyLog = new ImportDataModifyLog();
+        paramImportDataModifyLog.setImportDataId(importDataId);
+
+        List<ImportDataModifyLog> importDataModifyLogs = importDataModifyLogDao.selectList(paramImportDataModifyLog);
+
+        File file = FileUtil.generateFileforDownload(importDataModifyLogs,
+                        FileNameEnum.IMPORT_DATA_MODIFY_LOG.getDetailName());
+
         return null;
     }
 
