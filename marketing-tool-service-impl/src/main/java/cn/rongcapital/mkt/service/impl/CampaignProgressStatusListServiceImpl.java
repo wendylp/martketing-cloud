3359b69fd4c6a167f902113561c75eec49c5cf1a
jason <12481228@qq.com>
2016-06-28 12:47:02 +0800

    修复bug

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignProgressStatusListServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignProgressStatusListServiceImpl.java
@@ -97,13 +97,13 @@ public class CampaignProgressStatusListServiceImpl implements
 				cat.setCampaignHeadId(c.getId());
 				List<CampaignAudienceTarget> catList = campaignAudienceTargetDao.selectList(cat);
 				if(CollectionUtils.isNotEmpty(catList)){
-					List<String> campNameList = new ArrayList<String>();
+					List<String> audienceNameList = new ArrayList<String>();
 					for(CampaignAudienceTarget campaignAudienceTarget:catList) {
 						if(StringUtils.isNotBlank(campaignAudienceTarget.getName())) {
-							campNameList.add(campaignAudienceTarget.getName());
+							audienceNameList.add(campaignAudienceTarget.getName());
 						}
 					}
-					campaignProgressStatusListOut.setCampaignName(StringUtils.join(campNameList, ";"));
+					campaignProgressStatusListOut.setSegmentationName(StringUtils.join(audienceNameList, ";"));
 				}
 				
 				result.getData().add(campaignProgressStatusListOut);
