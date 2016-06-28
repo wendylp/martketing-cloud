6e5cbb4458d5b2c07b67f52558a9172f82c62037
nianjun.sun <nianjunsun@rongcapital.cn>
2016-06-28 16:26:09 +0800

    更新file enum的名字

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/FileNameEnum.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/FileNameEnum.java
@@ -2,14 +2,14 @@ package cn.rongcapital.mkt.common.enums;
 
 public enum FileNameEnum {
 
-    PARTY(0, "party"), 
-    POPULATION(1, "population"), 
-    CUSTOMER_TAGS(2, "customer_tags"), 
-    ARCH_POINT(3, "arch_point"), 
-    MEMBER(4, "member"), 
-    LOGIN(5, "login"), 
-    PAYMENT(6, "payment"), 
-    SHOPPING(7, "shopping"), 
+    PARTY(0, "data_party"), 
+    POPULATION(1, "data_population"), 
+    CUSTOMER_TAGS(2, "data_customer_tags"), 
+    ARCH_POINT(3, "data_arch_point"), 
+    MEMBER(4, "data_member"), 
+    LOGIN(5, "data_login"), 
+    PAYMENT(6, "data_payment"), 
+    SHOPPING(7, "data_shopping"), 
     IMPORT_DATA_MODIFY_LOG(8, "import_data_modify_log"),
 
     ;
