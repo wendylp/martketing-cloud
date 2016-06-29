ef32e2974e3175758f2a741d827627bf82058bd6
nianjun.sun <nianjunsun@rongcapital.cn>
2016-06-29 12:46:22 +0800

    越改越简单了 , 反倒是一开始多叉树遍历最难了.

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TaggroupSystemMenulistGetServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TaggroupSystemMenulistGetServiceImpl.java
@@ -25,8 +25,6 @@ import cn.rongcapital.mkt.vo.BaseOutput;
 @Service
 public class TaggroupSystemMenulistGetServiceImpl implements TaggroupSystemMenulistGetService {
 
-    private static final int ZERO_LEVEL = 1;
-
     private static final long TOP_LEVEL = -1L;
 
     @Autowired
@@ -37,44 +35,34 @@ public class TaggroupSystemMenulistGetServiceImpl implements TaggroupSystemMenul
 
         BaseOutput baseOutput = new BaseOutput(ApiErrorCode.SUCCESS.getCode(), ApiErrorCode.SUCCESS.getMsg(),
                         ApiConstant.INT_ZERO, null);
+        // 所有的数据 , 相当于一颗多叉树
+        List<Taggroup> trees = TaggroupDao.selectList(null);
 
-        List<Taggroup> taggroups = TaggroupDao.selectList(null);
-        List<List<Taggroup>> rawResultList = new ArrayList<>();
+        // 获取所有的top级别的节点
+        List<Taggroup> topTaggroups = getTopTaggroup(trees);
+        List<Map<String, Object>> resultList = new ArrayList<>();
 
-        // 1. 先获取到所有的最底层的叶子节点.我们可以这样理解,有多少叶子节点,就有多少种组合
-        List<Taggroup> leafNodes = getLeafNodes(taggroups);
-
-        if (CollectionUtils.isEmpty(leafNodes)) {
+        // 获取一级节点的子节点
+        if (CollectionUtils.isEmpty(topTaggroups)) {
             return baseOutput;
         } else {
-            // 通过每一个叶子节点获取到所有的叶子节点到root的组合
-            for (Taggroup leafNode : leafNodes) {
-                List<Taggroup> leafChain = new ArrayList<>();
-                leafChain = getGroupList(leafNode, taggroups);
-                rawResultList.add(leafChain);
-            }
-        }
-
-        List<Map<String, Object>> resultList = new ArrayList<>();
-        for (int i = 0; i < rawResultList.size(); i++) {
-            List<Taggroup> tmpList = rawResultList.get(i);
-            StringBuilder selectionName = new StringBuilder();
-            Map<String, Object> map = new HashMap<>();
-            // list中保存的是从叶子节点到根节点的组合,要倒着取值,从根节点取到叶子节点
-            for (int j = tmpList.size() - 1; j > -1; j--) {
-                selectionName.append(tmpList.get(j).getName());
-                if (j > 0) {
-                    selectionName.append(" - ");
+            for (int i = 0; i < topTaggroups.size(); i++) {
+                Taggroup topTaggroup = topTaggroups.get(i);
+                Map<String, Object> selectMap = new HashMap<>();
+                List<Map<String, Object>> childNodes = new ArrayList<>();
+                selectMap.put("select_name", topTaggroup.getName());
+                selectMap.put("id", topTaggroup.getId());
+                List<Taggroup> subNodes = getChildNodeByCurrentNode(topTaggroup, trees);
+                selectMap.put("child_count", subNodes.size());
+                for (Taggroup taggroup : subNodes) {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("select_name", taggroup.getName());
+                    map.put("id", taggroup.getId());
+                    childNodes.add(map);
                 }
-
-                if (j == 0) {
-                    map.put("select_count", getLeafNodeCount(tmpList.get(j), taggroups));
-                    map.put("id", tmpList.get(j).getId());
-                }
+                selectMap.put("child_nodes", childNodes);
+                resultList.add(selectMap);
             }
-            map.put("select_name", selectionName.toString());
-
-            resultList.add(map);
         }
 
         baseOutput.getData().addAll(resultList);
@@ -83,63 +71,39 @@ public class TaggroupSystemMenulistGetServiceImpl implements TaggroupSystemMenul
         return baseOutput;
     }
 
-    // 获取所有叶子节点
-    private List<Taggroup> getLeafNodes(List<Taggroup> trees) {
-        List<Taggroup> leafNodes = new ArrayList<>();
+    // 获取所有顶级节点,即parentId为-1的节点
+    private List<Taggroup> getTopTaggroup(List<Taggroup> trees) {
+        List<Taggroup> resultList = new ArrayList<>();
+
         if (CollectionUtils.isEmpty(trees)) {
-            // 只有根节点
-            return leafNodes;
+            return resultList;
         } else {
             for (Taggroup taggroup : trees) {
-                if (taggroup.getLevel().equals(ZERO_LEVEL)) {
-                    leafNodes.add(taggroup);
+                if (taggroup.getParentGroupId() == TOP_LEVEL) {
+                    resultList.add(taggroup);
                 }
             }
         }
 
-        return leafNodes;
+        return resultList;
     }
 
-    // 根据叶子节点获取该叶子节点到树根的整个序列
-    private List<Taggroup> getGroupList(Taggroup leafNode, List<Taggroup> trees) {
-        List<Taggroup> taggroups = new ArrayList<>();
-        taggroups.add(leafNode);
-        Taggroup previousParent = getParentNode(leafNode, trees);
-        while (!previousParent.getParentGroupId().equals(TOP_LEVEL)) {
-            taggroups.add(previousParent);
-            previousParent = getParentNode(previousParent, trees);
+    // 根据当前节点获取所有子节点
+    private List<Taggroup> getChildNodeByCurrentNode(Taggroup node, List<Taggroup> trees) {
+
+        List<Taggroup> resultList = new ArrayList<>();
+
+        if (node == null || CollectionUtils.isEmpty(trees)) {
+            return resultList;
         }
-        taggroups.add(previousParent);
 
-        return taggroups;
-    }
-
-    // 获取节点的父节点
-    private Taggroup getParentNode(Taggroup leafNode, List<Taggroup> trees) {
         for (Taggroup taggroup : trees) {
-            if (leafNode.getParentGroupId().compareTo(Long.valueOf(taggroup.getId())) == 0) {
-                return taggroup;
+            if (taggroup.getParentGroupId().equals(Long.valueOf(node.getId()))) {
+                resultList.add(taggroup);
             }
         }
 
-        return null;
-    }
-
-    // 根据节点获取所有直接子节点的数量
-    public int getLeafNodeCount(Taggroup node, List<Taggroup> trees) {
-        int count = 0;
-
-        if (CollectionUtils.isEmpty(trees)) {
-            return count;
-        } else {
-            for (Taggroup taggroup : trees) {
-                if (taggroup.getParentGroupId().equals(Long.valueOf(node.getId()))) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+        return resultList;
     }
 
 }
