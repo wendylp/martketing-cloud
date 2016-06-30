348bf2f83a4d00c087b6da862459450da8fb86e1
xu kun <xukunlucky@hotmail.com>
2016-06-30 16:10:32 +0800

    modify SegmentFilterCountIn.java

--- marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/in/SegmentFilterCountIn.java
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/in/SegmentFilterCountIn.java
@@ -9,15 +9,9 @@ import cn.rongcapital.mkt.vo.BaseInput;
 
 public class SegmentFilterCountIn extends BaseInput{
 
-    @NotEmpty
+    
     private String userToken;
 
-    
-    @NotEmpty
-    private String segment_head_id;
-	
-    @NotEmpty
-	private String group_index;
     
     private List<SegmentFilterCondition> conditions;
           
@@ -30,24 +24,7 @@ public class SegmentFilterCountIn extends BaseInput{
         this.userToken = userToken;
     }
     
-	
-    @JsonProperty("segment_head_id")
-	public String getSegment_head_id() {
-		return segment_head_id;
-	}
-
-	public void setSegment_head_id(String segment_head_id) {
-		this.segment_head_id = segment_head_id;
-	}
-	
-	@JsonProperty("group_index")
-	public String getGroup_index() {
-        return group_index;
-    }
-
-    public void setGroup_index(String group_index) {
-        this.group_index = group_index;
-    }
+    
     
     @JsonProperty("conditions")
     public List<SegmentFilterCondition> getConditions() {
