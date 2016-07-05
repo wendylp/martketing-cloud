c51cf8076e5c8afaab4241fa26d1e6d7725ae742
baiyunfeng <baiyunfeng@oneapm.com>
2016-07-05 16:55:19 +0800

    modify bug

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/GetH5PersonalListImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/GetH5PersonalListImpl.java
@@ -13,6 +13,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
