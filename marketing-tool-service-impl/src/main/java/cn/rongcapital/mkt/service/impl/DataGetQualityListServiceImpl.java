c6fc2b297ce33cbfdc6006ad7e6c52ac7d8882ae
nianjun.sun <nianjunsun@rongcapital.cn>
2016-07-07 17:26:02 +0800

    给import_data_history加个标示给云峰用

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetQualityListServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetQualityListServiceImpl.java
@@ -45,6 +45,7 @@ public class DataGetQualityListServiceImpl implements DataGetQualityListService 
         
         paramImportDataHistory.setOrderField("import_start_time");
         paramImportDataHistory.setOrderFieldType("DESC");
+        paramImportDataHistory.setStatus((byte)1);
 
         List<ImportDataHistory> importDataHistoryList = importDataHistoryDao.selectList(paramImportDataHistory);
         int totalCount = importDataHistoryDao.selectListCount(null);
