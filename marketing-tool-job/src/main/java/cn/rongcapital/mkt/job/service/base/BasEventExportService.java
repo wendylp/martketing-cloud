5a5c7f55578d6a2262a2a49e714ee1759f180ef7
nianjun.sun <nianjunsun@rongcapital.cn>
2016-07-13 16:05:26 +0800

    更新垃圾代码

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/base/BasEventExportService.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/base/BasEventExportService.java
@@ -4,5 +4,10 @@ import java.io.File;
 
 public interface BasEventExportService {
 
+    /**
+     * @功能简述 : 从data_xxx表中导出数据给BAS
+     * @author nianjun
+     * @return File
+     */
     public File exportData();
 }
--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/BasEventExportServiceImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/BasEventExportServiceImpl.java
@@ -1,5 +1,9 @@
 package cn.rongcapital.mkt.job.service.impl;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +17,17 @@ import cn.rongcapital.mkt.dao.DataPartyDao;
 import cn.rongcapital.mkt.dao.DataPaymentDao;
 import cn.rongcapital.mkt.dao.DataPopulationDao;
 import cn.rongcapital.mkt.dao.DataShoppingDao;
+import cn.rongcapital.mkt.job.service.base.BasEventExportService;
+import cn.rongcapital.mkt.po.DataParty;
+import cn.rongcapital.mkt.vo.out.BasEventOut;
 
 @Service
-public class BasEventExportServiceImpl {
+public class BasEventExportServiceImpl implements BasEventExportService {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    // 每次获取的数据量,不能一次把数据库的数据都导出来
+    private static final int LOOP_TIME = 1000;
 
     @Autowired
     private DataPartyDao dataPartyDao;
@@ -42,6 +52,23 @@ public class BasEventExportServiceImpl {
 
     @Autowired
     private DataShoppingDao dataShoppingDao;
-    
-    
+
+    @Override
+    public File exportData() {
+        return null;
+    }
+
+    // 获取
+    private List<BasEventOut> getBasData() {
+        // 1.获取dataParty中的数据数量,这也是最终导出的数据数量
+        DataParty paramDataPary = new DataParty();
+        // 导出未被删除的数据
+        paramDataPary.setStatus((byte) 0);
+        int totalCount = dataPartyDao.selectListCount(paramDataPary);
+        List<BasEventOut> basEventOuts = new ArrayList<>(totalCount);
+
+        return basEventOuts;
+    }
+
+
 }
