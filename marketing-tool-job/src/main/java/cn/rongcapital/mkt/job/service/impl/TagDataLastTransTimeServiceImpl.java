70048f1df5749751810af8f8fb0413360ec6c1c0
nianjun.sun <sunnianjun@rongcapital.cn>
2016-08-03 15:05:35 +0800

    提交打标签的代码

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/TagDataLastTransTimeServiceImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/TagDataLastTransTimeServiceImpl.java
@@ -7,8 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import cn.rongcapital.mkt.dao.DataPartyDao;
 import cn.rongcapital.mkt.dao.DataShoppingDao;
 import cn.rongcapital.mkt.job.service.base.TaskService;
+import cn.rongcapital.mkt.po.DataParty;
 import cn.rongcapital.mkt.po.ShoppingWechat;
 
 public class TagDataLastTransTimeServiceImpl implements TaskService {
@@ -17,6 +19,9 @@ public class TagDataLastTransTimeServiceImpl implements TaskService {
 
     @Autowired
     private DataShoppingDao dataShoppingDao;
+
+    @Autowired
+    private DataPartyDao dataPartyDao;
 
     @Override
     public void task(Integer taskId) {
@@ -29,10 +34,35 @@ public class TagDataLastTransTimeServiceImpl implements TaskService {
         return dataShoppingDao.selectListByLastTransTimeandWeChatInfo();
     }
 
-    public void tagData(List<ShoppingWechat> shoppingWechats) {
+    private void tagData(ShoppingWechat shoppingWechat) {
+
+    }
+
+    public void handleData(List<ShoppingWechat> shoppingWechats) {
         if (CollectionUtils.isEmpty(shoppingWechats)) {
             logger.info("购物记录中单个微信用户（公众号标识＋openid）最后一次购买（取订单号的消费时间记最后一次购买时间）的时间 因对应的数据不存在无法打标签");
             return;
+        }
+
+        for (ShoppingWechat shoppingWechat : shoppingWechats) {
+            DataParty paramDataParty = new DataParty();
+            paramDataParty.setWxmpCode(shoppingWechat.getPubId());
+            paramDataParty.setOpenid(shoppingWechat.getOpenId());
+            // 这里应该是一条数据
+            List<DataParty> dataParties = dataPartyDao.selectList(paramDataParty);
+            if (CollectionUtils.isEmpty(dataParties)) {
+                // 这其实就是个问题, 根据外键不能查数据 ?
+                logger.error("无法通过openId,pubId获取主数据");
+                continue;
+            } else {
+                if (dataParties.size() > 0) {
+                    logger.error("通过openId,pubId定位出超过一条的主数据了!openid={},pubid={}", shoppingWechat.getOpenId(),
+                                    shoppingWechat.getPubId());
+                    DataParty dataParty = dataParties.get(0);
+                    shoppingWechat.setDataPartyId(dataParty.getId());
+                    tagData(shoppingWechat);
+                }
+            }
         }
     }
 
