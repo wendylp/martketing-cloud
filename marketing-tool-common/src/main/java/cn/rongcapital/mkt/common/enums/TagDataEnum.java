0addfe25936709a3f2471bc610e1c11e9c9ad74d
nianjun.sun <sunnianjun@rongcapital.cn>
2016-08-03 14:13:02 +0800

    继续补充标签

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/TagDataEnum.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/TagDataEnum.java
@@ -10,7 +10,14 @@ public enum TagDataEnum {
     THREE_TIME(6, "3次"),
     MORE_THAN_FOUR_TIME(7, "4次以上"),
     ONE_TO_TWO_TIME(8, "1-2次"),
-    MORE_THAN_THREE_TIME(9, "3次以上")
+    MORE_THAN_THREE_TIME(9, "3次以上"),
+    LESS_THAN_FIFTY(10, "50元以下"),
+    FIFTY_ONE_TO_100(11, "51-100元"),
+    ONE_HUNDRED_TO_150(12, "101-150元"),
+    ONE_HUNDRED_FIFTY_TO_200(13, "151-200元"),
+    TWO_HUNDRED_TO_250(14, "201-250元"),
+    TWO_HUNDRED_FIFTY_TO_300(15, "251-300元"),
+    MORE_THAN_300(16, "301元以上"),
 
     ;
 
