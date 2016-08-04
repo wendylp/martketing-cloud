f1fe1b7a7a0a5f132349d813e5d092b6627065bc
ethan <liudisheng@rongcapital.cn>
2016-08-04 11:13:30 +0800

    add new filed to data party

--- marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/mongodb/DataParty.java
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/mongodb/DataParty.java
@@ -90,9 +90,28 @@ public class DataParty implements Serializable {
 
 	private Integer receiveCount;
 
-	private Integer totalCount;
-	
-	private Date payLastTime;
+	private Integer lastShoppingTime;
+
+	// 购物记录中单个微信用户（公众号标识＋openid）购买的总次数（订单号）
+	private Integer totalShoppingCount;
+
+	// 购物记录中单个微信用户（公众号标识＋openid）单月购买次数（订单号）
+	private Integer singleMonthShoppingCount;
+
+	// 支付记录中支付状态成功，单个微信用户（公众号标识＋openid）收入金额总额
+	private BigDecimal totalIncome;
+
+	// 支付记录中支付状态成功，单个微信用户（公众号标识＋openid）收入金额总额取平均数
+	private BigDecimal averageIncome;
+
+	// 公众号下，openid有购买记录的用户标记为是购买用户，其余的标记为不是购买用户 -- 是/否
+	private boolean isShoppingUser;
+
+	// 购物记录－购物渠道－官网字段／分销字段--（新增）交易渠道偏好－微盟－（旺铺、其它）
+	private String weimob;
+
+	// 购物记录的订单状态中，交易完成／交易关闭／待支付
+	private String paymentStatus;
 
 	public String getId() {
 		return id;
@@ -350,20 +369,67 @@ public class DataParty implements Serializable {
 		this.receiveCount = receiveCount;
 	}
 
-	public Integer getTotalCount() {
-		return totalCount;
-	}
+    public Integer getLastShoppingTime() {
+        return lastShoppingTime;
+    }
 
-	public void setTotalCount(Integer totalCount) {
-		this.totalCount = totalCount;
-	}
+    public void setLastShoppingTime(Integer lastShoppingTime) {
+        this.lastShoppingTime = lastShoppingTime;
+    }
 
-	public Date getPayLastTime() {
-		return payLastTime;
-	}
+    public Integer getTotalShoppingCount() {
+        return totalShoppingCount;
+    }
 
-	public void setPayLastTime(Date payLastTime) {
-		this.payLastTime = payLastTime;
-	}
-	
+    public void setTotalShoppingCount(Integer totalShoppingCount) {
+        this.totalShoppingCount = totalShoppingCount;
+    }
+
+    public Integer getSingleMonthShoppingCount() {
+        return singleMonthShoppingCount;
+    }
+
+    public void setSingleMonthShoppingCount(Integer singleMonthShoppingCount) {
+        this.singleMonthShoppingCount = singleMonthShoppingCount;
+    }
+
+    public BigDecimal getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(BigDecimal totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public BigDecimal getAverageIncome() {
+        return averageIncome;
+    }
+
+    public void setAverageIncome(BigDecimal averageIncome) {
+        this.averageIncome = averageIncome;
+    }
+
+    public boolean isShoppingUser() {
+        return isShoppingUser;
+    }
+
+    public void setShoppingUser(boolean shoppingUser) {
+        isShoppingUser = shoppingUser;
+    }
+
+    public String getWeimob() {
+        return weimob;
+    }
+
+    public void setWeimob(String weimob) {
+        this.weimob = weimob;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
 }
