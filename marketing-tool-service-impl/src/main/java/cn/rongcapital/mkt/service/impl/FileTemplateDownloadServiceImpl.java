688baaf109d3117f3ae0b3679b84e24bede73bc9
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-08-05 14:18:27 +0800

    修改模板下载时的压缩方式

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/FileTemplateDownloadServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/FileTemplateDownloadServiceImpl.java
@@ -10,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Yunfeng on 2016-6-24.
@@ -20,6 +23,7 @@ public class FileTemplateDownloadServiceImpl implements FileTemplateDownloadServ
     private Logger logger = LoggerFactory.getLogger(getClass());
 
 //    sudo tar -zcvPf /rc/downloads/1467362267553template.tar.zip /rc/templeteFiles/TYPE2_002_客户标签.csv /rc/templeteFiles/TYPE3_003_埋点统计.csv /rc/templeteFiles/TYPE4_004_会员卡记录.csv
+//    Todo:1修改文件压缩的方式，直接将文件压缩为zip文件。
     @Override
     public Object downloadFileTemplate(String templateIdList) {
         BaseOutput baseOutput = new BaseOutput(ApiErrorCode.DB_ERROR.getCode(), ApiErrorCode.DB_ERROR.getMsg(), ApiConstant.INT_ZERO, null);
@@ -35,26 +39,63 @@ public class FileTemplateDownloadServiceImpl implements FileTemplateDownloadServ
         File file = new File(ApiConstant.DOWNLOAD_TEMPLATE_FILE_DIR);
         templateFiles = getTemplateFiles(baseOutput, templateFiles, file);
         String[] idList = null;
+        File[] files = new File[7];
         if(templateIdList.contains(",")){
+            Integer fileIndex = 0;
             idList = templateIdList.split(",");
             for (String id : idList) {
                 //压缩文件有多个
                 String templateFileName = templateFiles[Integer.parseInt(id)].getAbsoluteFile().toString() + " ";
                 command += templateFileName;
+
+                //构造文件数组
+                files[fileIndex] = new File(templateFileName);
+                fileIndex++;
             }
         }else{
             //压缩文件有1个
             String templateFileName = templateFiles[Integer.parseInt(templateIdList)].getAbsoluteFile().toString() + "";
             command += templateFileName;
         }
-        logger.info("begin to execute command");
-        this.executeCommand(command);
+//        logger.info("begin to execute command");
+//        this.executeCommand(command);
+
+        zipTemplateFiles(files,generateFileName);
         baseOutput.setCode(ApiErrorCode.SUCCESS.getCode());
-        baseOutput.setMsg(command);
+        baseOutput.setMsg(generateFileName);
         DownloadFileName downloadFileName = new DownloadFileName();
         downloadFileName.setDownloadFileName(generateFileSimpleName);
         baseOutput.getData().add(downloadFileName);
         return baseOutput;
+    }
+
+    private void zipTemplateFiles(File[] files, String generateFileName) {
+        try {
+            logger.info("begin to execute zip action.");
+            OutputStream os = new BufferedOutputStream(new FileOutputStream(generateFileName + ".zip"));
+            ZipOutputStream zos = new ZipOutputStream(os);
+            byte[] buf = new byte[8192];
+            int len;
+            for(int i = 0; i<files.length; i++){
+                logger.info("begin to read :" + files[i]);
+                File file = files[i];
+                if(!file.isFile()) continue;
+                ZipEntry ze = new ZipEntry(file.getName());
+                zos.putNextEntry(ze);
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                while((len = bis.read(buf)) > 0 ){
+                    zos.write(buf,0,len);
+                }
+                zos.closeEntry();
+                logger.info("read " + files[i] + "close");
+            }
+            zos.closeEntry();
+            zos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private File[] getTemplateFiles(BaseOutput baseOutput, File[] templateFiles, File file) {
