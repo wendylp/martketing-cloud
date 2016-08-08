8f702400be9ea704501e0a6baa08aac96702f1fb
ethan <liudisheng@rongcapital.cn>
2016-08-08 19:43:55 +0800

    fix tag count bug

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentTagnameTagCountServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SegmentTagnameTagCountServiceImpl.java
@@ -7,10 +7,14 @@
  *************************************************/
 package cn.rongcapital.mkt.service.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.common.constant.ApiErrorCode;
+import cn.rongcapital.mkt.dao.TagDao;
+import cn.rongcapital.mkt.po.mongodb.DataParty;
+import cn.rongcapital.mkt.service.SegmentTagnameTagCountService;
+import cn.rongcapital.mkt.vo.BaseOutput;
+import heracles.data.common.annotation.ReadWrite;
+import heracles.data.common.util.ReadWriteType;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -18,14 +22,10 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import cn.rongcapital.mkt.common.constant.ApiConstant;
-import cn.rongcapital.mkt.common.constant.ApiErrorCode;
-import cn.rongcapital.mkt.po.mongodb.DataParty;
-import cn.rongcapital.mkt.po.mongodb.Tag;
-import cn.rongcapital.mkt.service.SegmentTagnameTagCountService;
-import cn.rongcapital.mkt.vo.BaseOutput;
-import heracles.data.common.annotation.ReadWrite;
-import heracles.data.common.util.ReadWriteType;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SegmentTagnameTagCountServiceImpl implements SegmentTagnameTagCountService{
@@ -33,6 +33,9 @@ public class SegmentTagnameTagCountServiceImpl implements SegmentTagnameTagCount
 //    private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private MongoTemplate mongoTemplate;
+
+	@Autowired
+	TagDao tagDao;
 	
 	@Override
 	@ReadWrite(type=ReadWriteType.READ)
@@ -41,26 +44,32 @@ public class SegmentTagnameTagCountServiceImpl implements SegmentTagnameTagCount
 	    BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
 	                    ApiErrorCode.SUCCESS.getMsg(),
 	                    ApiConstant.INT_ZERO,null);
+
+		List<Integer> idList = new ArrayList<>(ids.length);
+		for(String tagidStr : ids){
+			idList.add(Integer.valueOf(tagidStr));
+		}
+		List<cn.rongcapital.mkt.po.Tag> dbTags = tagDao.selectListByIdList(idList);
+		Map<String, cn.rongcapital.mkt.po.Tag> tagMap = new HashMap<>();
+		for (cn.rongcapital.mkt.po.Tag tag : dbTags) {
+			tagMap.put(tag.getId().toString(), tag);
+		}
+
 	    for(String tagidStr : ids){
 	        int tagId = Integer.parseInt(tagidStr);
-	        List<DataParty> restList =mongoTemplate.find(new Query(Criteria.where("tagList.tagId").is(tagId)),DataParty.class);
-	        Map<String,Object> map = new HashMap<String,Object>();
-	        if(CollectionUtils.isEmpty(restList)) {
-	        	map.put("tag_id","");
-	        	map.put("tag_count", 0);	
-	        	map.put("tag_name", "");
-	        } else {
-	        	map.put("tag_id",tagId);
-	        	int count=restList.size();	        
-	        	map.put("tag_count", count);	        
-	        	List<Tag> tagList = restList.get(0).getTagList();
-	        	for(Tag tag : tagList){
-	        		if(tagId == tag.getTagId().intValue()){
-	        			map.put("tag_name", tag.getTagName());
-	        			break;
-	        		}
-	        	}
+			cn.rongcapital.mkt.po.Tag tag = tagMap.get(tagidStr);
+			if (tag == null) {
+				continue;
+			}
+			List<DataParty> restList =mongoTemplate.find(new Query(Criteria.where("tagList.tagId").is(tagId)),DataParty.class);
+	        int tagCount = 0;
+			if(!CollectionUtils.isEmpty(restList)) {
+				tagCount = restList.size();
 	        }
+			Map<String,Object> map = new HashMap<String,Object>();
+			map.put("tag_id",tagId);
+			map.put("tag_name", tag.getName());
+			map.put("tag_count", tagCount);
             result.getData().add(map);
 	    }
 	    result.setTotal(result.getData().size());
