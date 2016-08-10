d1c84502985ffab7ec95aadf42fa03c6b2fb4202
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-08-10 09:47:56 +0800

    将数据源还原为7个

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainCountServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/DataGetMainCountServiceImpl.java
@@ -9,8 +9,6 @@ import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
-import cn.rongcapital.mkt.dao.WechatMemberDao;
-import cn.rongcapital.mkt.po.WechatMember;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,12 +30,6 @@ public class DataGetMainCountServiceImpl implements DataGetMainCountService {
     @Autowired
     private ImportTemplateDao importTemplateDao;
 
-    @Autowired
-    private WechatMemberDao wechatMemberDao;
-
-    private final Integer WECHAT_MD_TYPE = 8;
-    private final String WECHAT_NAME = "微信";
-
     @Override
     public Object getMainCount(String method, String userToken, String ver) {
 
@@ -58,24 +50,9 @@ public class DataGetMainCountServiceImpl implements DataGetMainCountService {
             resultList.add(map);
         }
 
-        //添加微信数据源，从而避免使用大的SQL
-        WechatMember wechatMember = new WechatMember();
-        wechatMember.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
-        Integer wechatMemberCount = wechatMemberDao.selectListCount(wechatMember);
-        Map<String,Object> wechatRelationResults = new HashMap<String,Object>();
-        wechatRelationResults.put("md_type",WECHAT_MD_TYPE);
-        wechatRelationResults.put("tag_name",WECHAT_NAME);
-        wechatRelationResults.put("count_rows",wechatMemberCount);
-        resultList.add(wechatRelationResults);
-
-        Integer dataSourceCount = 0;
-        for(Map<String,Object> map : resultList){
-            if((Integer)map.get("md_type")!= 0 && map.get("count_rows") != null && Long.valueOf(map.get("count_rows") + "")>0) dataSourceCount++;
-        }
-
-        result.setDataSourceCount(dataSourceCount);
         result.getData().addAll(resultList);
         result.setTotal(result.getData().size());
+
         return Response.ok().entity(result).build();
     }
 
