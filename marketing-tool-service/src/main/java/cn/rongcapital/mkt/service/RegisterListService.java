85446958a787d8aa7485c27de0400cdab5dd3070
Administrator <Administrator@WIN-71834AAOLM3>
2016-08-11 16:06:14 +0800

    RegisterListService

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/RegisterListService.java
@@ -0,0 +1,10 @@
+package cn.rongcapital.mkt.service;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+
+/**
+ * Created by Yunfeng on 2016-5-30.
+ */
+public interface RegisterListService {
+	BaseOutput getRegisterList();
+}
