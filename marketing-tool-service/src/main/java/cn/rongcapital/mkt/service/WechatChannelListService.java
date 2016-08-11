8c2636e866b680f1f620ab5a9af19a795a72117a
Administrator <Administrator@RUIXUE-08836641>
2016-08-11 16:06:11 +0800

    cc

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/WechatChannelListService.java
@@ -0,0 +1,16 @@
+/*************************************************
+ * @功能简述: 获取渠道列表
+ * @see MktApi：
+ * @author: zhaogyoing
+ * @version: 1.0
+ * @date：2016-08-11
+ *************************************************/
+package cn.rongcapital.mkt.service;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+
+
+public interface WechatChannelListService {
+	
+	public BaseOutput channelList();
+}
