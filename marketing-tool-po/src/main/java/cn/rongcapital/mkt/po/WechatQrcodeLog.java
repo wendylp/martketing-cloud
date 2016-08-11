157115cce40caeed129b10b5f7288288f449a66c
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-08-11 14:07:12 +0800

    修改新增的4张表的PO,DAO,Mapper

---
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/WechatQrcodeLog.java
@@ -0,0 +1,67 @@
+package cn.rongcapital.mkt.po;
+
+import cn.rongcapital.mkt.po.base.BaseQuery;
+
+import java.util.Date;
+
+public class WechatQrcodeLog extends BaseQuery {
+    private Long id;
+
+    private String sourceFilename;
+
+    private Integer totalRows;
+
+    private Byte success;
+
+    private Byte status;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSourceFilename() {
+        return sourceFilename;
+    }
+
+    public void setSourceFilename(String sourceFilename) {
+        this.sourceFilename = sourceFilename == null ? null : sourceFilename.trim();
+    }
+
+    public Integer getTotalRows() {
+        return totalRows;
+    }
+
+    public void setTotalRows(Integer totalRows) {
+        this.totalRows = totalRows;
+    }
+
+    public Byte getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Byte success) {
+        this.success = success;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
