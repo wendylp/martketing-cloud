672eceaa081ab575ccdd3ad5c1441f3f3e6582d8
yangyulin <yangyulin@rongcapital.cn>
2016-08-18 11:57:44 +0800

    update

--- marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ContactTemplateService.java
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ContactTemplateService.java
@@ -3,9 +3,9 @@ package cn.rongcapital.mkt.service;
 import javax.ws.rs.core.SecurityContext;
 
 import cn.rongcapital.mkt.vo.BaseOutput;
-import cn.rongcapital.mkt.vo.in.ContactTemplateIn;
+import cn.rongcapital.mkt.vo.in.ContactTempDIn;
 
 public interface ContactTemplateService {
-	BaseOutput copyContactTemplate(ContactTemplateIn body, SecurityContext securityContext);
+	BaseOutput copyContactTemplate(ContactTempDIn body, SecurityContext securityContext);
 
 }
