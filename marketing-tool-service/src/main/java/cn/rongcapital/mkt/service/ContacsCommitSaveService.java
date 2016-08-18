afb105b746dc567aab08d5c6633500d037c1f01d
chengjincheng <chengjincheng@rongcapital.cn>
2016-08-18 14:15:20 +0800

    联系人保存，获取，删除，下载地址相关操作

--- marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ContacsCommitSaveService.java
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ContacsCommitSaveService.java
@@ -1,7 +1,9 @@
 package cn.rongcapital.mkt.service;
 
+import java.util.Date;
+
 import cn.rongcapital.mkt.vo.BaseOutput;
-import cn.rongcapital.mkt.vo.in.ContactStatusUpdateIn;
+import cn.rongcapital.mkt.vo.in.ContactsCommitDelIn;
 import cn.rongcapital.mkt.vo.in.ContactsCommitSaveIn;
 
 /*
@@ -9,4 +11,10 @@ import cn.rongcapital.mkt.vo.in.ContactsCommitSaveIn;
  */
 public interface ContacsCommitSaveService {
 	BaseOutput contactsCommitSave(ContactsCommitSaveIn body);
+	
+	BaseOutput contactsCommitGet(Integer contact_id,Date commit_time);
+	
+	BaseOutput contactsCommitDel(ContactsCommitDelIn body);
+	
+	BaseOutput contactsCommitDownload(Integer contact_id,Date commit_time);
 }
