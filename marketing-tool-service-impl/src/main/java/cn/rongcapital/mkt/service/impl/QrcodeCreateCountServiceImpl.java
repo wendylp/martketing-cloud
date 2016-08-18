19c7946069811f5f19eef88d69ec4996afcc1bf8
congshulin <congshulin@rongcapital.cn>
2016-08-18 17:06:27 +0800

    代码合并

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/QrcodeCreateCountServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/QrcodeCreateCountServiceImpl.java
@@ -1,9 +1,14 @@
 package cn.rongcapital.mkt.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
+import cn.rongcapital.mkt.dao.WechatQrcodeLogDao;
 import cn.rongcapital.mkt.po.WechatQrcodeLog;
 import cn.rongcapital.mkt.service.QrcodeCreateCountService;
 import cn.rongcapital.mkt.vo.BaseOutput;
@@ -12,19 +17,25 @@ import heracles.data.common.util.ReadWriteType;
 
 @Service
 public class QrcodeCreateCountServiceImpl implements QrcodeCreateCountService {
+	@Autowired
+	WechatQrcodeLogDao qrcodeDao;
 
 	@Override
-	@ReadWrite(type=ReadWriteType.READ)
-	public BaseOutput getCreateCount(Long batch_id)
-	{
+	@ReadWrite(type = ReadWriteType.READ)
+	public BaseOutput getCreateCount(Integer batch_id) {
 		WechatQrcodeLog qlog = new WechatQrcodeLog();
-		qlog.setId(batch_id);
-		
-		BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
-				ApiErrorCode.SUCCESS.getMsg(),
-				ApiConstant.INT_ZERO,
-				null);
-		
+		qlog.setBatchId(batch_id);
+
+		BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(), ApiErrorCode.SUCCESS.getMsg(),
+				ApiConstant.INT_ZERO, null);
+
+		WechatQrcodeLog qrcodeLog = qrcodeDao.selectCountByBatchId(qlog);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("qrcode_succ", qrcodeLog.getSuccess());
+		map.put("qrcode_fail", qrcodeLog.getTotalRows() - qrcodeLog.getSuccess());
+		result.setTotal(1);
+		result.getData().add(map);
+
 		return result;
 	}
 }
