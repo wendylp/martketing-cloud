7cb6c50f2093f6f121b0c59ea4612ca09faced3e
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-08-22 14:31:06 +0800

    新增接口

--- marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ImportContactsDataToMDataService.java
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/ImportContactsDataToMDataService.java
@@ -1,7 +1,12 @@
 package cn.rongcapital.mkt.service;
 
+import cn.rongcapital.mkt.vo.BaseOutput;
+import cn.rongcapital.mkt.vo.in.ImportContactsDataIn;
+
 /**
  * Created by Yunfeng on 2016-8-22.
  */
 public interface ImportContactsDataToMDataService {
+
+    BaseOutput importContactsDataToMData(ImportContactsDataIn importContactsDataIn);
 }
