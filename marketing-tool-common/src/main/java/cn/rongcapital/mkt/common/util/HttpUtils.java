9f37dd8ef453f1dfecb6c5b48f6933caa0897cbe
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-08-25 15:12:09 +0800

    修改微信访问的域名

--- marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/util/HttpUtils.java
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/util/HttpUtils.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -18,11 +19,15 @@ import java.util.Map;
  */
 public class HttpUtils {
 
+    @Value("${runxue.h5.api.base.url}")
+    private String hostname;
     public static final String baseUrl = "http://h5plus.net/auth-template/api/?";
 
     public static HttpResponse requestH5Interface(Map<String, String> paramMap){
         HttpClient httpClient = new DefaultHttpClient();
-        String url = baseUrl;
+        HttpUtils httpUtils = new HttpUtils();
+        String url = "http://" + httpUtils.hostname;
+//        String url = baseUrl;
         for(String key : paramMap.keySet()){
             url = url + key + "=" + paramMap.get(key) + "&";
         }
