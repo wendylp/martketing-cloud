4b609c734929ce236d9123d7d95bbf710550909c
zhouqi <zhouqi@rongcapital.cn>
2016-08-29 15:16:36 +0800

    更新表结构加tag_group_id

---
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/Tag.java
@@ -0,0 +1,65 @@
+package cn.rongcapital.mkt.po;
+
+import java.util.Date;
+
+public class Tag {
+    private Integer id;
+
+    private String name;
+
+    private Byte status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String tagGroupId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getTagGroupId() {
+        return tagGroupId;
+    }
+
+    public void setTagGroupId(String tagGroupId) {
+        this.tagGroupId = tagGroupId == null ? null : tagGroupId.trim();
+    }
+}
