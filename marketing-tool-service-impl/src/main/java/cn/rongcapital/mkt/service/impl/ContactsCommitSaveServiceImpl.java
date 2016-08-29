b056f260b1448fe0f161d197b63cbe83f04d2b7c
李海光 <@DESKTOP-4FTUHGT>
2016-08-29 18:11:42 +0800

    修改显示总页数

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/ContactsCommitSaveServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/ContactsCommitSaveServiceImpl.java
@@ -116,8 +116,10 @@ public class ContactsCommitSaveServiceImpl implements ContacsCommitSaveService {
 				ApiConstant.INT_ZERO, null);
 
 		List<ContactList> list = contactDao.selectListByContactIdAndCommitTime(contact);
+		
 		if (!CollectionUtils.isEmpty(list)) {
-			result.setTotal(list.size());
+			int selectListCount = contactDao.selectListCount(contact);
+			result.setTotal(selectListCount);
 			for (ContactList item : list) {
 				Map<String, Object> map = new LinkedHashMap<>();
 				for(String filedName : filedNameList){
