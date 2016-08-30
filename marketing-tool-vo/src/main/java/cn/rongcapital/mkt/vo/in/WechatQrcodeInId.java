d8ab10c20fcdb197f9de0fe3a97cda129993d923
李海光 <@DESKTOP-4FTUHGT>
2016-08-30 14:59:07 +0800

    修改vo集成基类

--- marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/in/WechatQrcodeInId.java
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/in/WechatQrcodeInId.java
@@ -3,12 +3,19 @@ package cn.rongcapital.mkt.vo.in;
 import javax.validation.constraints.NotNull;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
-public class WechatQrcodeInId {
+import cn.rongcapital.mkt.vo.BaseInput;
+
+public class WechatQrcodeInId extends BaseInput{
 	
 	@NotNull
 	@JsonProperty("id")
 	private Integer id;
+	
+	@NotEmpty
+    @JsonProperty("user_token")
+    private String userToken;
 
 	public Integer getId() {
 		return id;
@@ -16,6 +23,14 @@ public class WechatQrcodeInId {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getUserToken() {
+		return userToken;
+	}
+
+	public void setUserToken(String userToken) {
+		this.userToken = userToken;
 	}
 	
 	
