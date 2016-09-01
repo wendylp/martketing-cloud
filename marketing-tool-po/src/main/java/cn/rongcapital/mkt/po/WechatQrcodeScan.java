b0a52208336ce04820ab2a6b391ceab58cedb593
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-09-01 12:19:37 +0800

    增加表：wechat_qrcode_scan

---
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/WechatQrcodeScan.java
@@ -0,0 +1,57 @@
+package cn.rongcapital.mkt.po;
+
+import java.util.Date;
+
+import cn.rongcapital.mkt.po.base.BaseQuery;
+
+public class WechatQrcodeScan extends BaseQuery{
+    private Integer id;
+
+    private String userId;
+
+    private String userHost;
+
+    private Integer qrcodeId;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getUserHost() {
+        return userHost;
+    }
+
+    public void setUserHost(String userHost) {
+        this.userHost = userHost == null ? null : userHost.trim();
+    }
+
+    public Integer getQrcodeId() {
+        return qrcodeId;
+    }
+
+    public void setQrcodeId(Integer qrcodeId) {
+        this.qrcodeId = qrcodeId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
