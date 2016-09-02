e1aded7e12b663eb91fc4b4d26caf28accdcb2b2
李海光 <@DESKTOP-4FTUHGT>
2016-09-02 14:19:05 +0800

    批量新增接口修改

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WeixinQrcodeBatchSaveServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WeixinQrcodeBatchSaveServiceImpl.java
@@ -5,6 +5,7 @@ package cn.rongcapital.mkt.service.impl;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -21,6 +22,8 @@ import cn.rongcapital.mkt.dao.WechatQrcodeDao;
 import cn.rongcapital.mkt.po.WechatQrcode;
 import cn.rongcapital.mkt.service.WeixinQrcodeBatchSaveService;
 import cn.rongcapital.mkt.vo.BaseOutput;
+import cn.rongcapital.mkt.vo.in.AssociationTag;
+import cn.rongcapital.mkt.vo.in.WechatQrcodeBatchSaveIn;
 import cn.rongcapital.mkt.vo.out.HomePageCalendarPopOut;
 
 /**
@@ -31,6 +34,7 @@ import cn.rongcapital.mkt.vo.out.HomePageCalendarPopOut;
 public class WeixinQrcodeBatchSaveServiceImpl implements WeixinQrcodeBatchSaveService {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	@Autowired
 	WechatQrcodeDao wechatQrcodeDao;
@@ -70,6 +74,48 @@ public class WeixinQrcodeBatchSaveServiceImpl implements WeixinQrcodeBatchSaveSe
 		
 		logger.debug("更新数据id:{}",wechatQrcode.getId());
 
+		return result;
+	}
+
+	@Override
+	public BaseOutput weixinQrcodeBatchSave(WechatQrcodeBatchSaveIn body) {
+		BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(), ApiErrorCode.SUCCESS.getMsg(),
+				ApiConstant.INT_ZERO, null);
+		WechatQrcode wechatQrcode = new WechatQrcode();
+		wechatQrcode.setBatchId(body.getBatchId());
+		
+		String expirationTime = body.getExpirationTime();
+		
+		if (expirationTime != null && !expirationTime.isEmpty()) {
+			try {
+				Calendar expirationTimeCalendar = Calendar.getInstance();
+				expirationTimeCalendar.setTime(simpleDateFormat.parse(expirationTime));
+				wechatQrcode.setExpirationTime(expirationTimeCalendar.getTime());
+			} catch (ParseException e) {
+				logger.error("转换expirationTime到日期出错,expirationTime = {} ", expirationTime);
+			}
+		}
+		
+		wechatQrcode.setComments(body.getComment());
+		
+		List<AssociationTag> associationTags = body.getAssociation_tags();
+		List<String> tagList = new ArrayList<String>();
+		
+		for(AssociationTag at : associationTags){
+			tagList.add(String.valueOf(at.getId()));
+		}
+		StringBuilder related = new StringBuilder();;
+		for(String tag :tagList){
+			related.append(tag);
+			related.append(";");
+		}
+		if(related != null &&related.length() > 0){
+			String relatedTags = related.toString().substring(0,related.length()-1);
+			wechatQrcode.setRelatedTags(relatedTags);
+		}
+		
+		wechatQrcodeDao.updataByBatchId(wechatQrcode);
+		
 		return result;
 	}
 
