2372fe52d06f4d351e656ad0712ff012ef92c93f
yinheng <yinheng@rongcapital.cn>
2016-09-02 15:53:13 +0800

    afasfasf

--- marketing-tool-biz-impl/src/main/java/cn/rongcapital/mkt/biz/impl/WechatQrcodeBizImpl.java
+++ marketing-tool-biz-impl/src/main/java/cn/rongcapital/mkt/biz/impl/WechatQrcodeBizImpl.java
@@ -198,7 +198,7 @@ public class WechatQrcodeBizImpl implements WechatQrcodeBiz {
 				baseOutput.setTotal(1);
 				List<Object> data = new ArrayList<Object>();
 				Map<String,Object> mapBack = new HashMap<String,Object>();
-				mapBack.put("scene_id", wechatQrcodeTicketBack.getId());
+				mapBack.put("scene_id", wechatQrcodeTicketBack.getId()+".jpg");
 				mapBack.put("url", wechatQrcodeTicketBack.getUrl());
 				data.add(mapBack);
 				baseOutput.setData(data);
@@ -287,7 +287,7 @@ public class WechatQrcodeBizImpl implements WechatQrcodeBiz {
 			List<Object> data = new ArrayList<Object>();
 			Map<String,Object> mapBack = new HashMap<String,Object>();
 			mapBack.put("id", wechatQrcode.getId());
-			mapBack.put("id", wechatQrcode.getQrcodePic());
+			mapBack.put("qrcodePic", wechatQrcode.getQrcodePic());
 			mapBack.put("createtime", wechatQrcode.getCreateTime());
 			data.add(mapBack);
 			baseOutput.setData(data);
