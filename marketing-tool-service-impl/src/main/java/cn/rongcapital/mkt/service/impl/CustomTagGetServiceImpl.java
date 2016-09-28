cc2ec8044e45e62351f92c3dfb1bd15ac89122a5
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-09-28 18:01:21 +0800

    修改自定义标签的相关功能

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CustomTagGetServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CustomTagGetServiceImpl.java
@@ -58,52 +58,6 @@ public class CustomTagGetServiceImpl implements CustomTagGetService {
         }
 
         return customTagOutput;
-//        if (customTagList != null && !customTagList.isEmpty()) {
-//            for (CustomTag tag : customTagList) {
-//                Map<String, Object> map = new HashMap<String, Object>();
-//                map.put("tag_id", tag.getId());
-//                map.put("tag_name", tag.getName());
-//                DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-//                String creatTime = "";
-//                if(null != tag.getCreateTime()) {
-//                	creatTime = format.format(tag.getCreateTime());
-//                }
-//                map.put("create_time", creatTime);
-//                map.put("cover_audience_count", tag.getCoverAudienceCount());
-//                map.put("tag_source","上传文件");
-//                result.getData().add(map);
-//            }
-//        }
-//
-//        Map<String, Object> columnNameA = new HashMap<>();
-//        Map<String, Object> columnNameB = new HashMap<>();
-//        Map<String, Object> columnNameC = new HashMap<>();
-//        Map<String, Object> columnNameD = new HashMap<>();
-//        columnNameA.put("col_id", 1);
-//        columnNameB.put("col_id", 2);
-//        columnNameC.put("col_id", 4);
-//        columnNameD.put("col_id", 3);
-//
-//
-//        columnNameA.put("col_code", "tag_name");
-//        columnNameB.put("col_code", "create_time");
-//        columnNameC.put("col_code", "cover_audience_count");
-//        columnNameD.put("col_code", "tag_source");
-//
-//        columnNameA.put("col_name", "标签名称");
-//        columnNameB.put("col_name", "添加时间");
-//        columnNameC.put("col_name", "覆盖人群");
-//        columnNameD.put("col_name", "标签来源");
-//
-//        List colNameList = new ArrayList();
-//        colNameList.add(columnNameA);
-//        colNameList.add(columnNameB);
-//        colNameList.add(columnNameC);
-//        colNameList.add(columnNameD);
-//
-//        result.setTotal(result.getData().size());
-//        result.setTotalCount(totalCount);
-//        result.setColNames(colNameList);
     }
 
     private void getShownColumnsInfo(CustomTagOutput customTagOutput) {
