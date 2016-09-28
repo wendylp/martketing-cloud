2d8e0eea0dd6b7eb46c016a29a5aa5ba583adcb0
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-09-28 21:32:31 +0800

    把mongodb中data_paty表市的值插入到tag_list

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SynchroMongodbCityServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SynchroMongodbCityServiceImpl.java
@@ -33,7 +33,6 @@ public class SynchroMongodbCityServiceImpl implements SynchroMongodbCityService{
 	 * 根据mid把市的值写入到tag_list
 	 * 
 	 * @param mid 
-	 * @param CITY 传入字符串"市"
 	 * @author shuiyangyang
 	 * @date 2016.09.28
 	 */
--- marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/SynchroMongodbCityService.java
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/SynchroMongodbCityService.java
@@ -13,7 +13,6 @@ public interface SynchroMongodbCityService {
 	 * 根据mid把市的值写入到tag_list
 	 * 
 	 * @param mid 
-	 * @param CITY 传入字符串"市"
 	 * @author shuiyangyang
 	 * @date 2016.09.28
 	 */
