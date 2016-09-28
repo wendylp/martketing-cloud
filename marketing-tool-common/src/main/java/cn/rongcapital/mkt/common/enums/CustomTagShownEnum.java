538a9af4ee1f7d0aff0afaca1942db4dbe7cf55c
baiyunfeng <baiyunfeng@rongcapital.cn>
2016-09-28 11:18:42 +0800

    添加自定义标签的逻辑显示的枚举

---
+++ marketing-tool-common/src/main/java/cn/rongcapital/mkt/common/enums/CustomTagShownEnum.java
@@ -0,0 +1,52 @@
+package cn.rongcapital.mkt.common.enums;
+
+/**
+ * Created by Yunfeng on 2016-9-28.
+ */
+public enum CustomTagShownEnum {
+
+//    columnNameA.put("col_code", "tag_name");
+//    columnNameB.put("col_code", "create_time");
+//    columnNameC.put("col_code", "cover_audience_count");
+//    columnNameD.put("col_code", "tag_source");
+
+    COLUMN_TAG_NAME(1,"tag_name","标签名称"),
+    COLUMN_CREATE_TIEM(2,"create_time","添加时间"),
+    COLUMN_TAG_SOURCE(3,"tag_source","标签来源"),
+    COLUMN_COVER_AUDIENCE(4,"cover_audience_count","覆盖人群")
+    ;
+
+    private Integer columnId;
+    private String columnCode;
+    private String columnName;
+
+    CustomTagShownEnum(Integer columnId, String columnCode, String columnName) {
+        this.columnId = columnId;
+        this.columnCode = columnCode;
+        this.columnName = columnName;
+    }
+
+    public Integer getColumnId() {
+        return columnId;
+    }
+
+    public void setColumnId(Integer columnId) {
+        this.columnId = columnId;
+    }
+
+    public String getColumnCode() {
+        return columnCode;
+    }
+
+    public void setColumnCode(String columnCode) {
+        this.columnCode = columnCode;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+}
