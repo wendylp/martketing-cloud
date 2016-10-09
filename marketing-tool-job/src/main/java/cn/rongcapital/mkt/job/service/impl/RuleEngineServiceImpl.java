9b79b158f2b0f9803894841d5aaacb1f61218ee1
congshulin <congshulin@rongcapital.cn>
2016-10-09 17:06:35 +0800

    增加打标签日志

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/RuleEngineServiceImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/RuleEngineServiceImpl.java
@@ -34,143 +34,145 @@ import okhttp3.Response;
 @PropertySource("classpath:${conf.dir}/application-api.properties")
 public class RuleEngineServiceImpl implements RuleEngineService {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private static final String BIZ_TYPE_CODE = "biz_type_code";
+	private static final String BIZ_TYPE_CODE = "biz_type_code";
 
-    private static final String BIZ_CODE = "biz_code";
+	private static final String BIZ_CODE = "biz_code";
 
-    private static final String DATAS = "datas";
+	private static final String DATAS = "datas";
 
-    private static final String KEY_ID = "keyid";
+	private static final String KEY_ID = "keyid";
 
-    @Autowired
-    @Qualifier("secondaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate2;
+	@Autowired
+	@Qualifier("secondaryJdbcTemplate")
+	protected JdbcTemplate jdbcTemplate2;
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
-    @Autowired
-    Environment env;
+	@Autowired
+	Environment env;
 
-    @Autowired
-    private SynchroMongodbCityService synchroMongodbCityService;
+	@Autowired
+	private SynchroMongodbCityService synchroMongodbCityService;
 
-    @Override
-    public Boolean requestRuleEngine(List<Integer> keyIds) {
-        boolean requestStatus = false;
-        try {
-            if (!CollectionUtils.isEmpty(keyIds)) {
-                for (Integer keyId : keyIds) {
-                    // 请求参数集合
-                    Map<String, Object> requestMap = new HashMap<String, Object>();
-                    requestMap.put(BIZ_TYPE_CODE, "maketing-system-tag");
-                    requestMap.put(BIZ_CODE, keyId);
-                    Map<String, Object> dataMap = new HashMap<>();
-                    dataMap.put(KEY_ID, keyId);
-                    requestMap.put(DATAS, dataMap);
-                    String url = env.getProperty("rule.engine.url");
-                    Response response = OkHttpUtil.requestByPost(url, RequestMediaType.JSON,
-                                    JsonUtils.toJson(requestMap));
-                	// 返回码
+	@Override
+	public Boolean requestRuleEngine(List<Integer> keyIds) {
+		boolean requestStatus = false;
+		try {
+			if (!CollectionUtils.isEmpty(keyIds)) {
+				for (Integer keyId : keyIds) {
+					// 请求参数集合
+					Map<String, Object> requestMap = new HashMap<String, Object>();
+					requestMap.put(BIZ_TYPE_CODE, "maketing-system-tag");
+					requestMap.put(BIZ_CODE, keyId);
+					Map<String, Object> dataMap = new HashMap<>();
+					dataMap.put(KEY_ID, keyId);
+					requestMap.put(DATAS, dataMap);
+					String url = env.getProperty("rule.engine.url");
+					Response response = OkHttpUtil.requestByPost(url, RequestMediaType.JSON,
+							JsonUtils.toJson(requestMap));
+					// 返回码
 					int code = response.code();
-					logger.info("----------调用规则规则引擎----id:?,返回code:?", keyId, code);
+					logger.info("----------调用规则规则引擎----id:" + keyId + "返回code:" + code);
 					requestStatus = (code > 200 && code <= 206);
-					logger.info("----------调用规则规则引擎----requestStatus:?",requestStatus);
-					
-                    logger.info("");
-                    if(requestStatus){
-                        //同步标签
-                        sychMongoData(keyId.toString()); 
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error("请求规则引擎出现异常：---------------->" + e.getMessage(), e);
-        }
-        logger.info("----------requestRuleEngine----end----");
-        return requestStatus;
-    }
+					logger.info("----------调用规则规则引擎----requestStatus:" + requestStatus);
 
+					logger.info("");
+					if (requestStatus) {
+						// 同步标签
+						sychMongoData(keyId.toString());
+					}
+				}
+			}
+		} catch (Exception e) {
+			logger.error("请求规则引擎出现异常：---------------->" + e.getMessage(), e);
+		}
+		logger.info("----------requestRuleEngine----end----");
+		return requestStatus;
+	}
 
-    private void sychMongoData(String keyId) {
-        try {
-            List<Tag> tagList = new ArrayList<>(); // 标签集合
-            // 查询主数据
-            Criteria criteria = Criteria.where("mid").is(Integer.valueOf(keyId));
-            // 业务码
-            String code = "maketing-system-tag";
-            Object[] param = {code};
-            // 获取参数
-            List<Map<String, Object>> bizType = jdbcTemplate2.queryForList(
-                            "SELECT ruleSetCode,ruleSetVersion FROM rsk_biztype WHERE code = ?", param);
-            Map<String, Object> map = bizType.get(0);
-            String ruleSetCode = (String) map.get("ruleSetCode"); // 集合code
-            String ruleSetVersion = map.get("ruleSetVersion").toString(); // 集合版本
-            Object[] searchParam = {keyId, code, ruleSetCode, ruleSetVersion}; // 查询参数
+	private void sychMongoData(String keyId) {
+		try {
+			List<Tag> tagList = new ArrayList<>(); // 标签集合
+			// 查询主数据
+			Criteria criteria = Criteria.where("mid").is(Integer.valueOf(keyId));
+			// 业务码
+			String code = "maketing-system-tag";
+			Object[] param = { code };
+			// 获取参数
+			List<Map<String, Object>> bizType = jdbcTemplate2
+					.queryForList("SELECT ruleSetCode,ruleSetVersion FROM rsk_biztype WHERE code = ?", param);
+			Map<String, Object> map = bizType.get(0);
+			String ruleSetCode = (String) map.get("ruleSetCode"); // 集合code
+			String ruleSetVersion = map.get("ruleSetVersion").toString(); // 集合版本
+			Object[] searchParam = { keyId, code, ruleSetCode, ruleSetVersion }; // 查询参数
 
-            // 查询结果
-            List<Map<String, Object>> resultLit = jdbcTemplate2.queryForList(
-                            "SELECT ruleCode,result FROM rsk_result WHERE bizCode = ?AND bizTypeCode = ? AND ruleSetCode = ? AND ruleSetVersion = ? GROUP BY ruleCode ORDER BY sortId,time DESC",
-                            searchParam);
-            for (Map<String, Object> resMap : resultLit) {
-            	if(resMap == null) continue;
-                String ruleCode = (String) resMap.get("ruleCode"); // 规则code
-                String result = (String) resMap.get("result"); // 返回结果
-                logger.info("查询结果为---------------------------------------："+"标签名称："+ruleCode+"标签值："+result);
-                // 结果为空，不进行后续打标签操作
-                if (result == null) {
-                    continue;
-                }
-                // 查询推荐标签
-                Criteria criteriaAll = Criteria.where("tag_name_eng").is(ruleCode);
-                TagRecommend tagRecommend = mongoTemplate.findOne(new Query(criteriaAll),
-                                cn.rongcapital.mkt.po.mongodb.TagRecommend.class);
-                // 判断是否数据有误
-                int size = tagRecommend.getTagList().size();
-                int index = Integer.valueOf(result);
-                if (size <= index) {
-                    continue;
-                }
-                // 映射标签值
-                String tagValue = tagRecommend.getTagList().get(index);
-                // 封装标签数据
-                // TODO 目前groupId值为1，后续要改为匹配对应的ID
-                Tag tag = new Tag(tagRecommend.getTagId(), tagRecommend.getTagName(),
-                                tagRecommend.getTagNameEng(), tagValue, 1);
-                tagList.add(tag);
-            }
-            //获取人员的属性标签
-            Map<String, Object> tagMap = synchroMongodbCityService.synchroMongodbCity(Integer.valueOf(keyId));
-            if(tagMap != null){
-            	 Tag cityTag = (Tag) tagMap.get("city");
-                 if(cityTag != null){
-                 	tagList.add(cityTag);
-                 }
-                 Tag sexTag = (Tag) tagMap.get("sex");
-                 if(sexTag != null){
-                 	tagList.add(sexTag);
-                 }
-                 Tag mediaTrenchGeneraTag = (Tag) tagMap.get("mediaTrenchGenera");
-                 if(mediaTrenchGeneraTag != null){
-                 	tagList.add(mediaTrenchGeneraTag);
-                 }
-                 Tag mediaNameTag = (Tag) tagMap.get("mediaName");
-                 if(mediaNameTag != null){
-                 	tagList.add(mediaNameTag);
-                 }
-                 logger.info("属性类标签查询结果为：------------------->"+tagMap);
-            }
-            if (!CollectionUtils.isEmpty(tagList)) {
-                // 更新插入
-                Update update = new Update().set("tag_list", tagList);
-                // logger.info("同步标签数据方法开始执行：----------->标签属性为，Mid:" + bizCode);
-                DataParty dp = mongoTemplate.findAndModify(new Query(criteria), update, DataParty.class);
-                logger.info("打标签操作执行结束：-------------------------->更新的人员为："+dp.getMid());
-            }
-        } catch (Exception e) {
-            logger.error("同步标签数据到mongo出现异常----------------->"+e.getMessage(),e);
-        } 
-    }
+			// 查询结果
+			List<Map<String, Object>> resultLit = jdbcTemplate2.queryForList(
+					"SELECT ruleCode,result FROM rsk_result WHERE bizCode = ?AND bizTypeCode = ? AND ruleSetCode = ? AND ruleSetVersion = ? GROUP BY ruleCode ORDER BY sortId,time DESC",
+					searchParam);
+			logger.info("resultLit查询结果为------------------------------------" + resultLit);
+
+			for (Map<String, Object> resMap : resultLit) {
+				if (resMap == null)
+					continue;
+				String ruleCode = (String) resMap.get("ruleCode"); // 规则code
+				String result = (String) resMap.get("result"); // 返回结果
+				logger.info("查询结果为---------------------------------------：" + "标签名称：" + ruleCode + "标签值：" + result);
+				// 结果为空，不进行后续打标签操作
+				if (result == null) {
+					continue;
+				}
+				// 查询推荐标签
+				Criteria criteriaAll = Criteria.where("tag_name_eng").is(ruleCode);
+				TagRecommend tagRecommend = mongoTemplate.findOne(new Query(criteriaAll),
+						cn.rongcapital.mkt.po.mongodb.TagRecommend.class);
+				// 判断是否数据有误
+				int size = tagRecommend.getTagList().size();
+				int index = Integer.valueOf(result);
+				if (size <= index) {
+					continue;
+				}
+				// 映射标签值
+				String tagValue = tagRecommend.getTagList().get(index);
+				// 封装标签数据
+				// TODO 目前groupId值为1，后续要改为匹配对应的ID
+				Tag tag = new Tag(tagRecommend.getTagId(), tagRecommend.getTagName(), tagRecommend.getTagNameEng(),
+						tagValue, 1);
+				tagList.add(tag);
+			}
+			// 获取人员的属性标签
+			Map<String, Object> tagMap = synchroMongodbCityService.synchroMongodbCity(Integer.valueOf(keyId));
+			if (tagMap != null) {
+				Tag cityTag = (Tag) tagMap.get("city");
+				if (cityTag != null) {
+					tagList.add(cityTag);
+				}
+				Tag sexTag = (Tag) tagMap.get("sex");
+				if (sexTag != null) {
+					tagList.add(sexTag);
+				}
+				Tag mediaTrenchGeneraTag = (Tag) tagMap.get("mediaTrenchGenera");
+				if (mediaTrenchGeneraTag != null) {
+					tagList.add(mediaTrenchGeneraTag);
+				}
+				Tag mediaNameTag = (Tag) tagMap.get("mediaName");
+				if (mediaNameTag != null) {
+					tagList.add(mediaNameTag);
+				}
+				logger.info("属性类标签查询结果为：------------------->" + tagMap);
+			}
+			if (!CollectionUtils.isEmpty(tagList)) {
+				// 更新插入
+				Update update = new Update().set("tag_list", tagList);
+				// logger.info("同步标签数据方法开始执行：----------->标签属性为，Mid:" + bizCode);
+				DataParty dp = mongoTemplate.findAndModify(new Query(criteria), update, DataParty.class);
+				logger.info("打标签操作执行结束：-------------------------->更新的人员为：" + dp.getMid());
+			}
+		} catch (Exception e) {
+			logger.error("同步标签数据到mongo出现异常----------------->" + e.getMessage(), e);
+		}
+	}
 }
