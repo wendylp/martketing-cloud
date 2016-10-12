9687c0a811521275872794f87672c90db40b2d6b
congshulin <congshulin@rongcapital.cn>
2016-10-12 20:27:54 +0800

    创建存放视图的表

---
+++ marketing-tool-po/src/main/java/cn/rongcapital/mkt/po/SysTagView.java
@@ -0,0 +1,35 @@
+package cn.rongcapital.mkt.po;
+
+import cn.rongcapital.mkt.po.base.BaseQuery;
+
+public class SysTagView extends BaseQuery {
+    private Integer id;
+
+    private String viewName;
+
+    private String viewDesc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName == null ? null : viewName.trim();
+    }
+
+    public String getViewDesc() {
+        return viewDesc;
+    }
+
+    public void setViewDesc(String viewDesc) {
+        this.viewDesc = viewDesc == null ? null : viewDesc.trim();
+    }
+}
