f3afe2ec2bf63e42473080c6a0a36b5c0f2a6843
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-10-26 14:55:05 +0800

    增加tag_id保持不变

--- marketing-tool-api/src/test/java/cn/rongcapital/mkt/unittest/service/AnalysisTagFileServiceImplTest.java
+++ marketing-tool-api/src/test/java/cn/rongcapital/mkt/unittest/service/AnalysisTagFileServiceImplTest.java
@@ -7,15 +7,21 @@
 package cn.rongcapital.mkt.unittest.service;
 
 
-import org.junit.Test;
+import java.io.IOException;
 
-//public class AnalysisTagFileServiceImplTest extends AbstractUnitTest {
-public class AnalysisTagFileServiceImplTest{
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.rongcapital.mkt.service.AnalysisTagFileService;
+import cn.rongcapital.mkt.unittest.AbstractUnitTest;
+
+public class AnalysisTagFileServiceImplTest extends AbstractUnitTest {
+//public class AnalysisTagFileServiceImplTest{
 	
-//	@Autowired
-//	private AnalysisTagFileService analysisTagFileService;
-//	
-//	public String path = "d:\\incake.xlsx";
+	@Autowired
+	private AnalysisTagFileService analysisTagFileService;
+	
+	public String path = "d:\\标签\\incake.xlsx";
 	
 	@Test
 	public void TestReadXlsx() {
