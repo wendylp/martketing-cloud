f044fc4616f73322d6870887ec97fbd4a229fe1c
yinheng <yinheng@rongcapital.cn>
2016-10-27 14:32:06 +0800

    该公众号暂时无法提供服务，请稍后再试  bug修改

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/wechat/api/MktWeChatMsgApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/wechat/api/MktWeChatMsgApi.java
@@ -49,16 +49,16 @@ public class MktWeChatMsgApi {
 	@POST
 	@Path("/mkt.weixin.qrcode.getMsgEvent/{appId}")
 	@Consumes({MediaType.TEXT_XML})
-	public BaseOutput getMsgEvent(String textxml,@QueryParam("msg_signature") String msg_signature,@QueryParam("timestamp") String timestamp, @QueryParam("nonce") String nonce, @QueryParam("signature") String signature, @QueryParam("openid") String openid, @PathParam("appId") String appId) throws JAXBException, AesException{		
+	public String getMsgEvent(String textxml,@QueryParam("msg_signature") String msg_signature,@QueryParam("timestamp") String timestamp, @QueryParam("nonce") String nonce, @QueryParam("signature") String signature, @QueryParam("openid") String openid, @PathParam("appId") String appId) throws JAXBException, AesException{		
 		logger.info("获取监听事件的消息     textxml:"+textxml);
 		logger.info("获取监听事件的消息参数:   msg_signature："+msg_signature+";timestamp:"+timestamp+";nonce:"+nonce+";signature:"+signature+";openid:"+openid+";appId:"+appId);
 		processReceiveMessageOfWeiXinBiz.getMsgLog(textxml, msg_signature, timestamp, nonce, signature, openid,appId);
-        BaseOutput baseOutput = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
+/*        BaseOutput baseOutput = new BaseOutput(ApiErrorCode.SUCCESS.getCode(),
                         ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO, null);
         List<Object> data = new ArrayList<Object>();
         data.add("success");
-        baseOutput.setData(data);
-		return baseOutput;		
+        baseOutput.setData(data);*/
+		return "success";		
 	}
 
 }
