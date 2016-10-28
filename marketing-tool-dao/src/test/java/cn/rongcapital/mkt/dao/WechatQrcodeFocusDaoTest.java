6b706e47899735786ba15b353eba089234a1a5b4
zhouqi <zhouqi@rongcapital.cn>
2016-10-28 10:23:51 +0800

    bug fixed for 二维码分析

--- marketing-tool-dao/src/test/java/cn/rongcapital/mkt/dao/WechatQrcodeFocusDaoTest.java
+++ marketing-tool-dao/src/test/java/cn/rongcapital/mkt/dao/WechatQrcodeFocusDaoTest.java
@@ -11,6 +11,8 @@
 package cn.rongcapital.mkt.dao;
 
 import cn.rongcapital.mkt.dao.testbase.AbstractUnitTest;
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.common.util.DateUtil;
 import cn.rongcapital.mkt.dao.WechatQrcodeFocusDao;
 
 import org.slf4j.Logger;
@@ -20,6 +22,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.Before;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.After;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -30,19 +37,34 @@ public class WechatQrcodeFocusDaoTest extends AbstractUnitTest {
     @Autowired
     private WechatQrcodeFocusDao wechatQrcodeFocusDao;
     
+    Map<String, Object> paraMap = new HashMap<String, Object>();
+    
     @Before  
     public void setUp() throws Exception {
         logger.info("测试: WechatQrcodeFocusDao 开始---------------------");
+               
+        paraMap.put("chCode", "1");
+        paraMap.put("wxAcct", "gh_4685d4eef135");
+    
+        //起始时间
+        String startDate = "2016-10-26 00:00:00";
+        String endDate = "2016-10-27 00:00:00";
+        paraMap.put("startTime", DateUtil.getDateFromString(startDate, ApiConstant.DATE_FORMAT_yyyy_MM_dd_HH_mm_ss));
+        paraMap.put("endTime", DateUtil.getDateFromString(endDate, ApiConstant.DATE_FORMAT_yyyy_MM_dd_HH_mm_ss));
     }
     
 	@Test
 	public void testGetLostFocusMax() {
 	    logger.info("测试方法: getLostFocusMax ");    
+	    Map<String, Object> lostFocusMax = wechatQrcodeFocusDao.getLostFocusMax(paraMap);
+	    logger.info(lostFocusMax.toString());
 	}
 	
 	@Test
 	public void testGetNewFocusMax() {
-	    logger.info("测试方法: getNewFocusMax ");    
+	    logger.info("测试方法: getNewFocusMax ");
+	    Map<String, Object> newFocusMax = wechatQrcodeFocusDao.getNewFocusMax(paraMap);
+	    logger.info(newFocusMax.toString());
 	}
 	
 	@Test
@@ -53,10 +75,16 @@ public class WechatQrcodeFocusDaoTest extends AbstractUnitTest {
 	@Test
 	public void testGetAddFocusMax() {
 	    logger.info("测试方法: getAddFocusMax ");    
+	    Map<String, Object> AddFocusMax = wechatQrcodeFocusDao.getAddFocusMax(paraMap);
+	    logger.info(AddFocusMax.toString());
 	}
 	
 	@Test
 	public void testGetAllFocusData() {
+	    
+	       
+        List<Map<String, Object>> analysisChdataMap= wechatQrcodeFocusDao.getAllFocusData(paraMap);        
+        logger.info(analysisChdataMap.toString());
 	    logger.info("测试方法: getAllFocusData ");    
 	}
 	
@@ -67,7 +95,11 @@ public class WechatQrcodeFocusDaoTest extends AbstractUnitTest {
 	
 	@Test
 	public void testGetAmountFocusMax() {
-	    logger.info("测试方法: getAmountFocusMax ");    
+	    logger.info("测试方法: getAmountFocusMax ");   
+	    
+	    Map<String, Object> amountFocusMax=wechatQrcodeFocusDao.getAmountFocusMax(paraMap);
+	    logger.info(amountFocusMax.toString());
+	    
 	}
 	
 	@Test
