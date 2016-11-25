47ab4df6b859397e5292aeff38c80d878e4c6abf
congshulin <congshulin@rongcapital.cn>
2016-11-25 18:04:35 +0800

    增加人群管理log

--- marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/AudienceCountTaskImpl.java
+++ marketing-tool-job/src/main/java/cn/rongcapital/mkt/job/service/impl/AudienceCountTaskImpl.java
@@ -3,6 +3,7 @@ package cn.rongcapital.mkt.job.service.impl;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.eclipse.jetty.util.log.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,21 +21,22 @@ public class AudienceCountTaskImpl implements TaskService {
 	@Autowired
 	private AudienceListPartyMapDao audienceListPartyMapDao;
 	private static int batchSize = 10;
-	
+
 	@Override
 	public void task(Integer taskId) {
+		Log.info("更新人群管理中人数Task----start----");
 		AudienceList audienceListT = new AudienceList();
 		audienceListT.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
 		int totalRecord = audienceListDao.selectListCount(audienceListT);
-		int totalPage = (totalRecord + batchSize -1) / batchSize;
-		for(int pageIndex = 1; pageIndex <= totalPage; pageIndex++) {
-			audienceListT = new AudienceList(pageIndex,batchSize);
+		int totalPage = (totalRecord + batchSize - 1) / batchSize;
+		for (int pageIndex = 1; pageIndex <= totalPage; pageIndex++) {
+			audienceListT = new AudienceList(pageIndex, batchSize);
 			audienceListT.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
 			List<AudienceList> audienceListList = audienceListDao.selectList(audienceListT);
-			if(CollectionUtils.isEmpty(audienceListList)) {
+			if (CollectionUtils.isEmpty(audienceListList)) {
 				break;
 			}
-			for(AudienceList audienceList:audienceListList) {
+			for (AudienceList audienceList : audienceListList) {
 				AudienceListPartyMap audienceListPartyMapT = new AudienceListPartyMap();
 				audienceListPartyMapT.setStatus(ApiConstant.TABLE_DATA_STATUS_VALID);
 				audienceListPartyMapT.setAudienceListId(audienceList.getId());
@@ -43,6 +45,7 @@ public class AudienceCountTaskImpl implements TaskService {
 				audienceListDao.updateById(audienceList);
 			}
 		}
+		Log.info("更新人群管理中人数Task----end----");
 	}
 
 }
