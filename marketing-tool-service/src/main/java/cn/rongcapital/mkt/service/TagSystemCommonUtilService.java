5eb28e27c5ce9738b5fd4b5db0f793437591cd0f
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-12-05 16:47:05 +0800

    与标签相关的公共service

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/TagSystemCommonUtilService.java
@@ -0,0 +1,14 @@
+package cn.rongcapital.mkt.service;
+
+public interface TagSystemCommonUtilService {
+    
+    /**
+     * 功能描述：根据标签tag_id获取标签覆盖率
+     * 任何异常都返回：0%
+     * 
+     * @param tagId
+     * @return
+     */
+    public String getTagCover(String tagId);
+
+}
