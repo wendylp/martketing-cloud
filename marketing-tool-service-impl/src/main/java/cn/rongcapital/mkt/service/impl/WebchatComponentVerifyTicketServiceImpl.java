72667ee1aa45a253a4d91d122784f57ee13b1573
congshulin <congshulin@rongcapital.cn>
2016-12-07 13:51:40 +0800

    测试事务

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WebchatComponentVerifyTicketServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/WebchatComponentVerifyTicketServiceImpl.java
@@ -46,7 +46,6 @@ public class WebchatComponentVerifyTicketServiceImpl implements WebchatComponent
 	}
 
 	@Override
-	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void insert(ComponentVerifyTicketIn componentVerifyTicketIn, String msg_signature, String timestamp,
 			String nonce) {
 		String appId = componentVerifyTicketIn.getAppId();
@@ -121,7 +120,8 @@ public class WebchatComponentVerifyTicketServiceImpl implements WebchatComponent
 	 * @return:
 	 * 
 	 */
-	private void updateStatusForWechat(String wxAcct, byte status) {
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	public void updateStatusForWechat(String wxAcct, byte status) {
 		WechatRegister wechatRegister = new WechatRegister();
 		wechatRegister.setWxAcct(wxAcct);
 		wechatRegister.setStatus(status);
