52f3713f433a07fac6bdefb8a74f65306ea2b836
lihaiguang <lihaiguang@rongcapital.cn>
2016-12-07 10:41:06 +0800

    微信fix相关修改

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SmsTargetAudienceListGetServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/SmsTargetAudienceListGetServiceImpl.java
@@ -3,13 +3,20 @@ package cn.rongcapital.mkt.service.impl;
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.common.enums.SmsTargetAudienceTypeEnum;
+import cn.rongcapital.mkt.common.jedis.JedisClient;
+import cn.rongcapital.mkt.common.jedis.JedisException;
 import cn.rongcapital.mkt.dao.AudienceListDao;
+import cn.rongcapital.mkt.dao.AudienceListPartyMapDao;
 import cn.rongcapital.mkt.dao.SegmentationHeadDao;
 import cn.rongcapital.mkt.po.AudienceList;
+import cn.rongcapital.mkt.po.AudienceListPartyMap;
 import cn.rongcapital.mkt.po.SegmentationHead;
 import cn.rongcapital.mkt.service.SmsTargetAudienceListGetService;
 import cn.rongcapital.mkt.vo.out.SmsTargetAudienceListOut;
 import cn.rongcapital.mkt.vo.out.SmsTargetAudienceOut;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -22,15 +29,18 @@ import java.util.List;
 @Service
 public class SmsTargetAudienceListGetServiceImpl implements SmsTargetAudienceListGetService{
 
+    private static Logger logger = LoggerFactory.getLogger(SmsTargetAudienceListOut.class);
     @Autowired
     private SegmentationHeadDao segmentationHeadDao;
 
     @Autowired
     private AudienceListDao audienceListDao;
+    
+    @Autowired
+    private AudienceListPartyMapDao audienceListPartyMapDao;
 
     @Override
     public SmsTargetAudienceListOut getSmsTargetAudienceList() {
-
         SmsTargetAudienceListOut smsTargetAudienceListOut = new SmsTargetAudienceListOut(ApiErrorCode.SUCCESS.getCode(),ApiErrorCode.SUCCESS.getMsg(), ApiConstant.INT_ZERO);
 
         SegmentationHead paramSegmentationHead = new SegmentationHead();
@@ -44,6 +54,16 @@ public class SmsTargetAudienceListGetServiceImpl implements SmsTargetAudienceLis
                 smsTargetAudienceOut.setId(Long.valueOf(segmentationHead.getId()));
                 smsTargetAudienceOut.setType(SmsTargetAudienceTypeEnum.SMS_TARGET_SEGMENTATION.getTypeCode());
                 smsTargetAudienceOut.setName(segmentationHead.getName());
+                String segmentKey = SegmentCalcServiceImpl.KEY_PREFIX_SEGMENTCOVERID + segmentationHead.getId();
+                Long segmentCount = 0L;
+                try {
+                    segmentCount = JedisClient.scard(SegmentCalcServiceImpl.REDISDB_INDEX, segmentKey);
+                } catch (JedisException e) {
+                    logger.error("get segmentCount error {}" , e);
+                    e.printStackTrace();
+                }
+                logger.info("segmentCount is " + segmentCount);
+                smsTargetAudienceOut.setCount(segmentCount);
                 smsTargetAudienceListOut.getSmsTargetAudienceOutList().add(smsTargetAudienceOut);
             }
         }
@@ -57,6 +77,12 @@ public class SmsTargetAudienceListGetServiceImpl implements SmsTargetAudienceLis
             for(AudienceList audienceList : audienceListList){
                 SmsTargetAudienceOut smsTargetAudienceOut = new SmsTargetAudienceOut();
                 smsTargetAudienceOut.setId(Long.valueOf(audienceList.getId()));
+                //audience count 
+                AudienceListPartyMap audienceListPartyMap = new AudienceListPartyMap();
+                audienceListPartyMap.setAudienceListId(audienceList.getId());
+                Integer audienceListPartyCount = audienceListPartyMapDao.selectListCount(audienceListPartyMap);
+                logger.info("audienceListPartyCount is " + audienceListPartyCount);
+                smsTargetAudienceOut.setCount(audienceListPartyCount.longValue());
                 smsTargetAudienceOut.setType(SmsTargetAudienceTypeEnum.SMS_TARGET_AUDIENCE.getTypeCode());
                 smsTargetAudienceOut.setName(audienceList.getAudienceName());
                 smsTargetAudienceListOut.getSmsTargetAudienceOutList().add(smsTargetAudienceOut);
