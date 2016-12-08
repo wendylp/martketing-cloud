6970f9bfabc4a92e10e3dc13522610160720ab3d
shanjingqi <shanjingqi@rongcapital.cn>
2016-12-08 13:56:52 +0800

    MC v1.6 interface mkt.material.coupon.releaseGeneral

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
@@ -9,6 +9,43 @@
  *************************************************/
 package cn.rongcapital.mkt.material.coupon.api;
 
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.resteasy.plugins.validation.hibernate.ValidateRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.service.MaterialCouponReleaseGeneralService;
+import cn.rongcapital.mkt.vo.BaseOutput;
+
+@Component
+@Path(ApiConstant.API_PATH)
+@Produces({ MediaType.APPLICATION_JSON })
+@ValidateRequest
+@PropertySource("classpath:${conf.dir}/application-api.properties")
 public class CouponApi {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private MaterialCouponReleaseGeneralService materialCouponReleaseGeneralService;
+
+	@GET
+	@Path("/mkt.material.coupon.releaseGeneral")
+	public BaseOutput releaseGeneral(@NotEmpty @QueryParam("method") String method,
+			@NotEmpty @QueryParam("user_token") String userToken, @NotEmpty @QueryParam("ver") String version,
+			@NotNull @QueryParam("id") Long id) {
+
+		return materialCouponReleaseGeneralService.releaseGeneralById(id, userToken, version);
+	}
 
 }
