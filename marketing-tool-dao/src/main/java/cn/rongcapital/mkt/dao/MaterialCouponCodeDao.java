56d6203dbba9addea5dd946329c3719e968ebe2b
xiexiaoliang <xiexiaoliang@rongcapital.cn>
2016-12-08 14:04:57 +0800

    MCDEV-106 优惠券获取列表

--- marketing-tool-dao/src/main/java/cn/rongcapital/mkt/dao/MaterialCouponCodeDao.java
+++ marketing-tool-dao/src/main/java/cn/rongcapital/mkt/dao/MaterialCouponCodeDao.java
@@ -10,39 +10,28 @@
 
 package cn.rongcapital.mkt.dao;
 
-import java.util.Map;
-
 import cn.rongcapital.mkt.dao.base.BaseDao;
 import cn.rongcapital.mkt.po.MaterialCouponCode;
 
 public interface MaterialCouponCodeDao extends BaseDao<MaterialCouponCode>{
-	
-	//自定义扩展
-	/**
-	 * 父类方法无法满足需求时使用,需在mapper.xml中扩展
-	 * 查询对象list;
-	 * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
-	 * @param paramMap
-	 * @return list
-	 */
-	//List<T> selectListBycustomMap(Map<String,Object> paramMap);
-	
-	//自定义扩展
-	/**
-	 * 父类方法无法满足需求时使用,需在mapper.xml中扩展
-	 * 查询对象总数
-	 * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
-	 * @param paramMap
-	 * @return list
-	 */
-	//List<T> selectListCountBycustomMap(Map<String,Object> paramMap);
     
+    //自定义扩展
     /**
-     * @功能描述: 获取
-     * @param paramMap 包含优惠码ID，券码，用户
-     * @return MaterialCouponCode
-     * @author xie.xiaoliang
-     * @since 2016年12月8日
+     * 父类方法无法满足需求时使用,需在mapper.xml中扩展
+     * 查询对象list;
+     * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
+     * @param paramMap
+     * @return list
      */
-    MaterialCouponCode selectByIdCodeUser(Map<String,Object> paramMap);
+    //List<T> selectListBycustomMap(Map<String,Object> paramMap);
+    
+    //自定义扩展
+    /**
+     * 父类方法无法满足需求时使用,需在mapper.xml中扩展
+     * 查询对象总数
+     * 自定义条件查询,只要不为NULL与空则为条件,属性值之间and连接
+     * @param paramMap
+     * @return list
+     */
+    //List<T> selectListCountBycustomMap(Map<String,Object> paramMap);
 }
