2feda73df116d7b64dbac2d85833e05160b5b99c
zhuxuelong <zhuxuelong@ruixuesoft.com>
2016-12-08 17:18:59 +0800

    update

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/MaterialCouponGeneralGetServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/MaterialCouponGeneralGetServiceImpl.java
@@ -8,10 +8,16 @@
  *************************************************/
 package cn.rongcapital.mkt.service.impl;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.MaterialCouponDao;
+import cn.rongcapital.mkt.po.MaterialCoupon;
 import cn.rongcapital.mkt.service.MaterialCouponGeneralGetService;
 import cn.rongcapital.mkt.vo.BaseOutput;
 
@@ -23,10 +29,20 @@ public class MaterialCouponGeneralGetServiceImpl implements MaterialCouponGenera
 
     @Override
     public BaseOutput getMaterialCouponGeneral(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        BaseOutput result =
+                        new BaseOutput(ApiErrorCode.SUCCESS.getCode(), ApiErrorCode.SUCCESS.getMsg(),
+                                        ApiConstant.INT_ZERO, null);
+        result.setTotal(ApiConstant.INT_ZERO);
+        result.setTotalCount(ApiConstant.INT_ZERO);
+        MaterialCoupon po = new MaterialCoupon();
+        po.setId(id);
+        po.setStatus((byte) 0);
+        List<MaterialCoupon> dataList = materialCouponDao.selectList(po);
+        if (CollectionUtils.isNotEmpty(dataList)) {
+            result.setTotal(ApiConstant.INT_ONE);
+            result.setTotalCount(ApiConstant.INT_ONE);
+            result.getData().add(dataList.get(0));
+        }
+        return result;
     }
-
- 
-
 }
