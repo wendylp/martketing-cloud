5055c970ee126d6af4088ad0e2e1972d4156622a
zhuxuelong <zhuxuelong@ruixuesoft.com>
2016-12-08 16:22:06 +0800

    获取当前系统时间

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
@@ -16,12 +16,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import org.jboss.resteasy.plugins.validation.hibernate.ValidateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.service.CouponCodeListService;
+import cn.rongcapital.mkt.service.MaterialCouponGetSystemTimeService;
 import cn.rongcapital.mkt.vo.BaseOutput;
 
 @Component
@@ -33,6 +36,9 @@ public class CouponApi {
     @Autowired
     private CouponCodeListService couponCodeListService;
 
+    @Autowired
+    private MaterialCouponGetSystemTimeService materialCouponGetSystemTimeService;
+    
     /**
      * @author guozhenchao
      * @功能简述: 获取优惠码列表接口
@@ -46,5 +52,18 @@ public class CouponApi {
             @NotEmpty @QueryParam("ver") String ver, @NotNull @QueryParam("id") Long id,
             @QueryParam("index") Integer index, @QueryParam("size") Integer size) {
         return couponCodeListService.couponCodeList(id, index, size);
+    }
+    
+    /**
+     * @功能简述: 获取当前系统时间
+     * @return BaseOutput
+     * @author zhuxuelong
+     */
+    @GET
+    @Path("/mkt.material.coupon.systemtime")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public BaseOutput getSystemTime(@NotEmpty @QueryParam("user_token") String userToken,
+                    @NotEmpty @QueryParam("ver") String ver) {
+        return materialCouponGetSystemTimeService.getSystemTime();
     }
 }
