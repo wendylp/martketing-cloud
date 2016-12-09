def11a83a54901aecf0639bfc9804c522cace4e1
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-12-09 18:19:37 +0800

    api拆分

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/api/MktApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/api/MktApi.java
@@ -44,34 +44,6 @@ import cn.rongcapital.mkt.vo.ImgAsset;
 import cn.rongcapital.mkt.vo.ImgtextHostIn;
 import cn.rongcapital.mkt.vo.LoginInput;
 import cn.rongcapital.mkt.vo.ModifyInput;
-import cn.rongcapital.mkt.vo.in.Audience;
-import cn.rongcapital.mkt.vo.in.CampaignBodyCreateIn;
-import cn.rongcapital.mkt.vo.in.CampaignDeleteIn;
-import cn.rongcapital.mkt.vo.in.CampaignHeadCreateIn;
-import cn.rongcapital.mkt.vo.in.CampaignHeadUpdateIn;
-import cn.rongcapital.mkt.vo.in.CustomTagDeleteIn;
-import cn.rongcapital.mkt.vo.in.SegmentBodyUpdateIn;
-import cn.rongcapital.mkt.vo.in.SegmentCountFilterIn;
-import cn.rongcapital.mkt.vo.in.SegmentCreUpdateIn;
-import cn.rongcapital.mkt.vo.in.SegmentFilterSumIn;
-import cn.rongcapital.mkt.vo.in.SegmentHeadCreateIn;
-import cn.rongcapital.mkt.vo.in.SegmentHeadDeleteIn;
-import cn.rongcapital.mkt.vo.in.SegmentHeadUpdateIn;
-import cn.rongcapital.mkt.vo.in.SegmentTagUpdateIn;
-import cn.rongcapital.mkt.vo.in.TagGroupsListIn;
-import cn.rongcapital.mkt.vo.in.TagListSecondarySearchIn;
-import cn.rongcapital.mkt.vo.in.TagSystemFlagSetIn;
-import cn.rongcapital.mkt.vo.out.CampaignBodyCreateOut;
-import cn.rongcapital.mkt.vo.out.CampaignBodyGetOut;
-import cn.rongcapital.mkt.vo.out.CampaignBodyItemAudienceSearchOut;
-import cn.rongcapital.mkt.vo.out.CampaignConvertChartListOut;
-import cn.rongcapital.mkt.vo.out.CampaignCustomSourceListOut;
-import cn.rongcapital.mkt.vo.out.CampaignHeaderGetOut;
-import cn.rongcapital.mkt.vo.out.CampaignNodeItemListOut;
-import cn.rongcapital.mkt.vo.out.CampaignProfileOut;
-import cn.rongcapital.mkt.vo.out.CampaignProgressStatusCountOut;
-import cn.rongcapital.mkt.vo.out.SegmentPublishstatusListOut;
-import cn.rongcapital.mkt.vo.out.SerarchTagGroupTagsOut;
 
 @Component
 @Path(ApiConstant.API_PATH)
@@ -86,12 +58,6 @@ public class MktApi {
 	private ModifyPasswdService modifyPasswdService;
 
 	@Autowired
-	private SegmentHeaderCreateService segmentHeaderService;
-
-	@Autowired
-	private SegmentPublishStatusCountService segmentPublishStatusCountService;
-
-	@Autowired
 	private DeleteImgTextAssetService deleteImgTextAssetService;
 
 	@Autowired
@@ -101,58 +67,19 @@ public class MktApi {
 	private ImgtextHostService imgtextHostService;
 
 	@Autowired
-	private SegmentHeaderGetService segmentHeaderGetService;
-
-	@Autowired
-	private SegmentBodyUpdateService segmentBodyUpdateService;
-
-	@Autowired
 	private GetImgtextAssetMenulistService getImgtextAssetMenulistService;
-
-	@Autowired
-	private SegmentTagnameTagListService segmentTagnameTagListService;
-
-	@Autowired
-	private SegmentTagkeyTagListService segmentTagkeyTagListService;
-
-	@Autowired
-	private SegmentTagnameTagValueService segmentTagnameTagValueService;
-
-	@Autowired
-	private TagSystemTagcountService tagSystemTagcountService;
-
-	@Autowired
-	private SegmentBodyGetService segmentBodyGetService;
-
-	@Autowired
-	private CustomTagGetService customTagGetService;
-
-	@Autowired
-	private CustomTagDeleteService customTagDeleteService;
 
 	@Autowired
 	private GetImgtextCountService getImgtextCountService;
 
 	@Autowired
-	private TagSystemListGetService tagSystemListGetService;
-
-	@Autowired
 	private TaskGetListService taskGetListService;
-
-	@Autowired
-	private TagDownloadCustomAudienceService tagDownloadCustomAudienceService;
 
 	@Autowired
 	private HomePageCalendarListService homePageCalendarListService;
 
 	@Autowired
 	private HomePageCalendarPopService homePageCalendarPopService;
-
-	@Autowired
-	private SegmentDetailGetService segmentDetailGetService;
-
-	@Autowired
-	private SegmentSecondaryTaglistSearchService segmentSecondaryTaglistSearchService;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
    
@@ -228,46 +155,6 @@ public class MktApi {
 		return getImgtextCountService.getImgtextAssetCount();
 	}
 
-	/**
-	 * @功能简述: 根据id获取segment header
-	 * @param: SegmentHeadIn
-	 *             body, SecurityContext securityContext, String segmentId
-	 * @return: Object
-	 */
-	@GET
-	@Path("/mkt.segment.header.get")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public BaseOutput segmentHeaderGet(@NotEmpty @QueryParam("user_token") String userToken,
-			@NotEmpty @QueryParam("ver") String ver, @NotEmpty @QueryParam("segment_head_id") String segmentId) {
-		return segmentHeaderGetService.segmentHeaderGet(userToken, ver, segmentId);
-	}
-
-	/**
-	 * @功能简述: 创建segment header
-	 * @param: SegmentHeadIn
-	 *             body, SecurityContext securityContext
-	 * @return: Object
-	 */
-	@POST
-	@Path("/mkt.segment.header.create")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public BaseOutput segmentHeaderCreate(@Valid SegmentHeadCreateIn body, @Context SecurityContext securityContext) {
-		return segmentHeaderService.segmentHeaderCreate(body, securityContext);
-	}
-
-	/**
-	 * @功能简述: 查询不同发布状态下的segment数量
-	 * @param: String
-	 *             userToken, String ver
-	 * @return: Object
-	 */
-	@GET
-	@Path("/mkt.segment.publishstatus.count.get")
-	public Object segmentPublishstatusCount(@NotEmpty @QueryParam("user_token") String userToken,
-			@NotEmpty @QueryParam("ver") String ver) throws Exception {
-		return segmentPublishStatusCountService.segmentPublishstatusCount(userToken, ver);
-	}
-
 
 	/**
 	 * @功能描述: 登录接口
@@ -318,60 +205,6 @@ public class MktApi {
 	}
 
 	/**
-	 * @功能描述:查询系统推荐标签列表
-	 * @Param: String method, String userToken
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.segment.tagname.taglist.get")
-	public BaseOutput getSysRecommendedTagList(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken) {
-		// segmentTagnameTagListService.getSysRecommendedTagList();
-		return segmentTagnameTagListService.getMongoTagRecommendList(method, userToken);
-	}
-
-	/**
-	 * @功能简述: 编辑segment body
-	 * @param: SegmentBodyUpdateIn
-	 *             body, SecurityContext securityContext
-	 * @return: Object
-	 */
-	@POST
-	@Path("/mkt.segment.body.update")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public Object segmentBodyUpdate(@Valid SegmentBodyUpdateIn body, @Context SecurityContext securityContext) {
-		return segmentBodyUpdateService.segmentBodyUpdate(body, securityContext);
-	}
-
-
-	/**
-	 * @功能描述:查询细分的详细信息
-	 * @Param: String method, String userToken
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.segment.detail.get")
-	public BaseOutput getSegmentDetail(@NotEmpty @QueryParam("method") String method,
-									   @NotEmpty @QueryParam("user_token") String userToken,
-	                                   @NotEmpty @QueryParam("ver") String ver,
-	                                   @NotNull  @QueryParam("id") Long id) {
-		return segmentDetailGetService.getSegmentDetail(id);
-	}
-
-	/**
-	 * @功能简述: 模糊搜索受众细分标签值的列表(二级搜索)
-	 * @param:
-	 * @return: Object
-	 */
-	@POST
-	@Path("/mkt.segment.secondary.taglist.search")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public BaseOutput searchSegmentSecondaryTaglist(TagListSecondarySearchIn tagListSecondarySearchIn, @Context SecurityContext securityContext) {
-		return segmentSecondaryTaglistSearchService.searchSegmentSecondaryTaglist(tagListSecondarySearchIn);
-	}
-
-
-	/**
 	 * @功能简述: 获取后台任务列表
 	 * @author nianjun
 	 * @param:
@@ -413,130 +246,6 @@ public class MktApi {
         return taskGetListService.updateTaskListStatus();
     }
 	
-	/*
-	 * @功能简述: 根据关键字查询出系统最末级标签名称列表
-	 * 
-	 * @param method
-	 * 
-	 * @param userToken
-	 * 
-	 * @param tagGroupName
-	 * 
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.segment.tagkey.taglist.get")
-	public BaseOutput getLastTagByKey(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken, @QueryParam("tag_group_name") String tagGroupName) {
-		//segmentTagkeyTagListService.getLastTagByKey(tagGroupName);
-		return segmentTagkeyTagListService.getMongoTagRecommendByLike(tagGroupName);
-	}
-
-	/**
-	 * @功能简述: 根据系统最末级标签组ID查询出标签内容列表
-	 * @param method
-	 * @param userToken
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.segment.tagname.tagvalue.get")
-	public BaseOutput getTagValueByGroupId(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken,
-			@NotEmpty @QueryParam("tag_group_id") String tagGroupId) {
-		//segmentTagnameTagValueService.getTagValueByGroupId(tagGroupId);
-		return segmentTagnameTagValueService.getMongoTagValueByTagId(tagGroupId);
-	}
-
-
-	/**
-	 * @功能简述: 获取受众细分body信息
-	 * @param userToken
-	 * @param segmentHeadId
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.segment.body.get")
-	public BaseOutput getSegmentBody(@NotEmpty @QueryParam("user_token") String userToken,
-			@NotEmpty @QueryParam("segment_head_id") String segmentHeadId) {
-		return segmentBodyGetService.getSegmentBody(userToken, segmentHeadId);
-	}
-
-	/**
-	 * @功能简述: 获取系统标签总数量
-	 * @param method
-	 * @param userToken
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.tag.system.tagcount.get")
-	public BaseOutput getTagcount(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken) {
-		//tagSystemTagcountService.getTagcount(method, userToken);
-		return tagSystemTagcountService.getMonggTagcount(method, userToken);
-	}
-
-	/**
-	 * @功能简述 : 获取自定义标签列表
-	 * @param: String
-	 *             method, String userToken, Integer index, Integer size
-	 * @return: Object
-	 */
-	@GET
-	@Path("/mkt.tag.custom.list.get")
-	public BaseOutput getCustomTagList(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken, @QueryParam("index") Integer index,
-			@QueryParam("size") Integer size) {
-		return customTagGetService.getCustomTagList(method, userToken, index, size);
-	}
-
-	/**
-	 * @功能简述 : 删除某个自定义标签
-	 * @param: String
-	 *             method, String userToken, Integer tag_id
-	 * @return: Object
-	 */
-	@POST
-	@Path("mkt.tag.custom.delete")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public BaseOutput deleteCustomTag(@Valid CustomTagDeleteIn body, @Context SecurityContext securityContext) {
-		return customTagDeleteService.deleteCustomTag(body);
-	}
-
-	/**
-	 * @功能简述 : 根据自定义标签下载覆盖的人群
-	 * @param: String
-	 *             userToken, Integer tag_id
-	 * @return: Object
-	 */
-	@GET
-	@Path("mkt.tag.custom.audience.download")
-	@Consumes({ MediaType.APPLICATION_JSON })
-	public BaseOutput downloadCustomAudience(@NotEmpty @QueryParam("method") String method,
-			@NotNull @QueryParam("tag_id") String tagId) {
-		return tagDownloadCustomAudienceService.downloadCustomAudience(tagId);
-	}
-
-
-	/**
-	 * 获取系统标签内容列表
-	 * 
-	 * @param method
-	 * @param userToken
-	 * @param tagGroupId
-	 * @param index
-	 * @param size
-	 * @return BaseOutput
-	 */
-	@GET
-	@Path("/mkt.tag.system.list.get")
-	public BaseOutput getTagcountByParentGroupId(@NotEmpty @QueryParam("method") String method,
-			@NotEmpty @QueryParam("user_token") String userToken,
-			@NotNull @QueryParam("tag_group_id") String tagGroupId, @QueryParam("index") Integer index,
-			@QueryParam("size") Integer size) {
-		//tagSystemListGetService.getTagcount(method, userToken, tagGroupId, index, size);
-		return tagSystemListGetService.getMongoTagList(method, userToken, tagGroupId, index, size);
-	}
-
 	/**
 	 * 统计出当月日历日被客户标记当月定时的活动，按启动时间算
 	 *
