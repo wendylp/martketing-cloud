0173a285255122befe637f9024e848bd6218ddaa
shuiyangyang <shuiyangyang@rongcapital.cn>
2016-12-13 12:06:42 +0800

    实现接口：mkt.tag.custom.taxonomy.save

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/TagCustomTaxonomySaveService.java
@@ -0,0 +1,18 @@
+package cn.rongcapital.mkt.service;
+
+import javax.ws.rs.core.SecurityContext;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+import cn.rongcapital.mkt.vo.in.TagCustomTaxonomySaveIn;
+
+public interface TagCustomTaxonomySaveService {
+    /**
+     * 功能描述：创建自定义分类
+     * 接口：mkt.tag.custom.taxonomy.save
+     * 
+     * @param body
+     * @param securityContext
+     * @return
+     */
+    public BaseOutput tagCustomTaxonomySave(TagCustomTaxonomySaveIn body,SecurityContext securityContext);
+}
