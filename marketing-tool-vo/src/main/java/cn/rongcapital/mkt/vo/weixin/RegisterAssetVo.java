f2888579306d8aaa072f91957f066f45137364fa
yinheng <yinheng@rongcapital.cn>
2016-12-16 15:41:56 +0800

    活动编排获取公众号信息 添加accest_id

--- marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/weixin/RegisterAssetVo.java
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/weixin/RegisterAssetVo.java
@@ -2,11 +2,21 @@ package cn.rongcapital.mkt.vo.weixin;
 
 public class RegisterAssetVo {
 
+	private Integer asset_id;
+	
 	private String asset_name;
 	
 	private String wx_acct;
 	
 	private String headerImage;
+
+	public Integer getAsset_id() {
+		return asset_id;
+	}
+
+	public void setAsset_id(Integer asset_id) {
+		this.asset_id = asset_id;
+	}
 
 	public String getAsset_name() {
 		return asset_name;
