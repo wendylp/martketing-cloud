67948865a1963c565bebe8ba76ffafc545286882
guozhenchao <guozhenchao@guozhenchao-PC>
2016-12-19 13:46:26 +0800

    保存接口状态判断

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/material/coupon/service/impl/CouponSaveServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/material/coupon/service/impl/CouponSaveServiceImpl.java
@@ -112,6 +112,12 @@ public class CouponSaveServiceImpl implements CouponSaveService {
                 baseOutput.setMsg(ApiErrorCode.DB_ERROR.getMsg());
                 return baseOutput;
             }
+            //优惠码未生成完不能编辑
+            if(MaterialCouponReadyStatusType.UNREADY.getCode().equals(coupon.getReadyStatus())){
+                baseOutput.setCode(ApiErrorCode.BIZ_ERROR_MATERIAL_COUPOON_CODE_ERROR.getCode());
+                baseOutput.setMsg(ApiErrorCode.BIZ_ERROR_MATERIAL_COUPOON_CODE_ERROR.getMsg());
+                return baseOutput;
+            }
             // 只有未投放才能编辑
             if (!MaterialCouponStatusEnum.UNUSED.getCode().equals(coupon.getCouponStatus())) {
                 baseOutput.setCode(ApiErrorCode.BIZ_ERROR_MATERIAL_COUPOON_UPDATE_ERROR.getCode());
