f6d19d43a977fc4ff4302556367651869187e2c6
shangchunming <shangchunming@rongcapital.cn>
2017-01-04 15:39:35 +0800

    事件类型

---
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/event/EventType.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cn.rongcapital.mkt.event;
+
+/**
+ * @author shangchunming
+ *
+ */
+public enum EventType {
+
+	/**
+	 * 营销事件
+	 */
+	MARKETING,
+
+	/**
+	 * 系统事件
+	 */
+	SYSTEM;
+
+}
