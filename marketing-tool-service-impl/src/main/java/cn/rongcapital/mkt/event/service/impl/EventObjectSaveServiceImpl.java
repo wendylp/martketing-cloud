87cfb8fbea926494847996e59c1b4b2e90fbb450
zhuxuelong <zhuxuelong@ruixuesoft.com>
2017-01-10 14:28:45 +0800

    更新事件实现

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/event/service/impl/EventObjectSaveServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/event/service/impl/EventObjectSaveServiceImpl.java
@@ -9,21 +9,26 @@
  *************************************************/
 package cn.rongcapital.mkt.event.service.impl;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
-
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.common.constant.ApiErrorCode;
 import cn.rongcapital.mkt.dao.event.EventObjectDao;
 import cn.rongcapital.mkt.event.po.EventObject;
 import cn.rongcapital.mkt.event.service.EventObjectSaveService;
+import cn.rongcapital.mkt.event.vo.in.EventObjecAttribure;
 import cn.rongcapital.mkt.event.vo.in.EventObjectVo;
 import cn.rongcapital.mkt.vo.BaseOutput;
+
+import com.alibaba.fastjson.JSON;
 
 @Service
 public class EventObjectSaveServiceImpl implements EventObjectSaveService {
@@ -41,17 +46,26 @@ public class EventObjectSaveServiceImpl implements EventObjectSaveService {
         EventObject eventObjPo = new EventObject();
         eventObjPo.setCode(event.getCode());
         eventObjPo.setStatus((byte) 0);
+        // 客体对象标识重复判断
         if (CollectionUtils.isNotEmpty(eventObjectDao.selectList(eventObjPo))) {
-            logger.error("event object code {} already exist.", eventObjPo.getCode());
+            logger.error("param is {}, event object code {} already exist.", event, eventObjPo.getCode());
             return new BaseOutput(ApiErrorCode.BIZ_ERROR_EVENT_OBJECT_CODE_ALREADY_EXIST.getCode(),
                     ApiErrorCode.BIZ_ERROR_EVENT_OBJECT_CODE_ALREADY_EXIST.getMsg(), ApiConstant.INT_ZERO, null);
         }
-
         eventObjPo.setName(event.getName());
         eventObjPo.setInstanceNameLabel(event.getInstanceNameLabel());
         eventObjPo.setInstanceNameProp(event.getInstanceNameProp());
-        if (CollectionUtils.isNotEmpty(event.getAttributes())) {
-            eventObjPo.setAttributes(JSON.toJSONString(event.getAttributes()));
+
+        List<EventObjecAttribure> attributes = event.getAttributes();
+        if (CollectionUtils.isNotEmpty(attributes)) {
+            // 客体对象属性重复判断
+            Set<String> attriSet = attributes.stream().map(EventObjecAttribure::getName).collect(Collectors.toSet());
+            if (attriSet.size() != attributes.size()) {
+                logger.error("param is {}, event object attribute is duplicated.", event);
+                return new BaseOutput(ApiErrorCode.BIZ_ERROR_EVENT_OBJECT_ATTRIBUTE_DUPLICATED.getCode(),
+                        ApiErrorCode.BIZ_ERROR_EVENT_OBJECT_ATTRIBUTE_DUPLICATED.getMsg(), ApiConstant.INT_ZERO, null);
+            }
+            eventObjPo.setAttributes(JSON.toJSONString(attributes));
         }
         eventObjPo.setSystemObject(false);
         eventObjectDao.insert(eventObjPo);
