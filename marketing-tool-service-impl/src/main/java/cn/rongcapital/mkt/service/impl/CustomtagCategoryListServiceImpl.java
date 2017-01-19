8d93e085b2cb12f975fdd6f9c408b37cfbd64114
shuiyangyang <shuiyangyang@rongcapital.cn>
2017-01-19 15:15:24 +0800

    实现接口：mkt.customtag.category.list

---
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CustomtagCategoryListServiceImpl.java
@@ -0,0 +1,60 @@
+package cn.rongcapital.mkt.service.impl;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.rongcapital.mkt.common.constant.ApiConstant;
+import cn.rongcapital.mkt.common.constant.ApiErrorCode;
+import cn.rongcapital.mkt.mongodao.MongoCustomTagCategoryDao;
+import cn.rongcapital.mkt.mongodao.MongoCustomTagDao;
+import cn.rongcapital.mkt.po.mongodb.CustomTagCategory;
+import cn.rongcapital.mkt.service.CustomtagCategoryListService;
+import cn.rongcapital.mkt.vo.BaseOutput;
+import cn.rongcapital.mkt.vo.out.CustomTagCategoryOut;
+
+@Service
+public class CustomtagCategoryListServiceImpl implements CustomtagCategoryListService {
+
+    @Autowired
+    MongoCustomTagCategoryDao mongoCustomTagCategoryDao;
+
+    @Autowired
+    MongoCustomTagDao mongoCustomTagDao;
+
+    /**
+     * 功能描述：自定义分类列表
+     * 接口：mkt.customtag.category.list
+     * 
+     * @return
+     */
+    @Override
+    public BaseOutput customtagCategoryListGet() {
+
+        BaseOutput result = new BaseOutput(ApiErrorCode.SUCCESS.getCode(), ApiErrorCode.SUCCESS.getMsg(),
+                ApiConstant.INT_ZERO, null);
+
+        CustomTagCategory customTagCategory = new CustomTagCategory();
+
+        List<CustomTagCategory> customTagCategoryLists = mongoCustomTagCategoryDao.find(customTagCategory);
+
+        if (CollectionUtils.isNotEmpty(customTagCategoryLists)) {
+            result.setTotal(customTagCategoryLists.size());
+            
+            for (CustomTagCategory cTagCategory : customTagCategoryLists) {
+                // 获取有效自定义标签个数
+                long customTagCount = mongoCustomTagDao.validCustomTagCount(cTagCategory.getChildrenCustomTagList());
+
+                CustomTagCategoryOut customTagCategoryOut =
+                        new CustomTagCategoryOut(cTagCategory.getCustomTagCategoryId(),
+                                cTagCategory.getCustomTagCategoryName(), cTagCategory.getLevel(), customTagCount);
+                result.getData().add(customTagCategoryOut);
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/CustomtagCategoryListService.java
@@ -0,0 +1,13 @@
+package cn.rongcapital.mkt.service;
+
+import cn.rongcapital.mkt.vo.BaseOutput;
+
+public interface CustomtagCategoryListService {
+    /**
+     * 功能描述：自定义分类列表
+     * 接口：mkt.customtag.category.list
+     * 
+     * @return
+     */
+    public BaseOutput customtagCategoryListGet();
+}
