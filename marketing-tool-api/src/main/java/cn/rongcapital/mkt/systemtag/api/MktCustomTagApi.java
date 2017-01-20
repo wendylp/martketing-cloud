d7511292d2f4f0c82434601a4dec3ca8399d87bc
shuiyangyang <shuiyangyang@rongcapital.cn>
2017-01-20 11:17:23 +0800

    实现接口：mkt.customtag.list

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/systemtag/api/MktCustomTagApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/systemtag/api/MktCustomTagApi.java
@@ -9,6 +9,9 @@
  *************************************************/
 package cn.rongcapital.mkt.systemtag.api;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,6 +27,7 @@ import org.springframework.stereotype.Component;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.service.CustomtagCategoryListService;
+import cn.rongcapital.mkt.service.CustomtagListService;
 import cn.rongcapital.mkt.vo.BaseOutput;
 
 @Component
@@ -32,12 +36,16 @@ import cn.rongcapital.mkt.vo.BaseOutput;
 @ValidateRequest
 public class MktCustomTagApi {
     private Logger logger = LoggerFactory.getLogger(getClass());
-    
+
     @Autowired
     private CustomtagCategoryListService customtagCategoryListService;
 
+    @Autowired
+    private CustomtagListService customtagListService;
+
     /**
      * 功能描述：自定义分类列表
+     * 
      * 接口：mkt.customtag.category.list
      * 
      * @return
@@ -45,8 +53,27 @@ public class MktCustomTagApi {
     @GET
     @Path("/mkt.customtag.category.list")
     public BaseOutput customtagCategoryListGet(@NotEmpty @QueryParam("method") String method,
-                                     @NotEmpty @QueryParam("user_token") String userToken) {
+            @NotEmpty @QueryParam("user_token") String userToken) {
         return customtagCategoryListService.customtagCategoryListGet();
+    }
+    
+
+    /**
+     * 功能描述：标签列表(分页， 分类名称展示)
+     * 
+     * @param customTagCategoryId
+     * @param index
+     * @param size
+     * @return
+     */
+    @GET
+    @Path("/mkt.customtag.list")
+    public BaseOutput customtagListGet(@NotEmpty @QueryParam("method") String method,
+            @NotEmpty @QueryParam("user_token") String userToken,
+            @NotEmpty @QueryParam("custom_tag_category_id") String customTagCategoryId,
+            @DefaultValue("1") @Min(1) @QueryParam("index") Integer index,
+            @DefaultValue("10") @Min(1) @Max(100) @QueryParam("size") Integer size) {
+        return customtagListService.customtagListGet(customTagCategoryId,index,size);
     }
 
 }
