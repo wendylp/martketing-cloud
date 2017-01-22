0db1a6ef0ecbd6e1c309abee69edf473826702f5
congshulin <congshulin@rongcapital.cn>
2017-01-22 15:28:25 +0800

    MCDEV-162 创建、编辑自定义标签

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/systemtag/api/MktCustomTagApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/systemtag/api/MktCustomTagApi.java
@@ -9,14 +9,19 @@
  *************************************************/
 package cn.rongcapital.mkt.systemtag.api;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.jboss.resteasy.plugins.validation.hibernate.ValidateRequest;
@@ -28,52 +33,69 @@ import org.springframework.stereotype.Component;
 import cn.rongcapital.mkt.common.constant.ApiConstant;
 import cn.rongcapital.mkt.service.CustomtagCategoryListService;
 import cn.rongcapital.mkt.service.CustomtagListService;
+import cn.rongcapital.mkt.tag.service.CustomtagCategoryCreateService;
+import cn.rongcapital.mkt.tag.vo.in.CustomTagCategoryIn;
 import cn.rongcapital.mkt.vo.BaseOutput;
 
 @Component
 @Path(ApiConstant.API_PATH)
-@Produces({MediaType.APPLICATION_JSON})
+@Produces({ MediaType.APPLICATION_JSON })
 @ValidateRequest
 public class MktCustomTagApi {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Autowired
-    private CustomtagCategoryListService customtagCategoryListService;
+	@Autowired
+	private CustomtagCategoryListService customtagCategoryListService;
 
-    @Autowired
-    private CustomtagListService customtagListService;
+	@Autowired
+	private CustomtagListService customtagListService;
 
-    /**
-     * 功能描述：自定义分类列表
-     * 
-     * 接口：mkt.customtag.category.list
-     * 
-     * @return
-     */
-    @GET
-    @Path("/mkt.customtag.category.list")
-    public BaseOutput customtagCategoryListGet(@NotEmpty @QueryParam("method") String method,
-            @NotEmpty @QueryParam("user_token") String userToken) {
-        return customtagCategoryListService.customtagCategoryListGet();
-    }
-    
+	@Autowired
+	private CustomtagCategoryCreateService customtagCategoryCreateService;
+	/**
+	 * 功能描述：自定义分类列表
+	 * 
+	 * 接口：mkt.customtag.category.list
+	 * 
+	 * @return
+	 */
+	@GET
+	@Path("/mkt.customtag.category.list")
+	public BaseOutput customtagCategoryListGet(@NotEmpty @QueryParam("method") String method,
+			@NotEmpty @QueryParam("user_token") String userToken) {
+		return customtagCategoryListService.customtagCategoryListGet();
+	}
 
-    /**
-     * 功能描述：标签列表(分页， 分类名称展示)
-     * 
-     * @param customTagCategoryId
-     * @param index
-     * @param size
-     * @return
-     */
-    @GET
-    @Path("/mkt.customtag.list")
-    public BaseOutput customtagListGet(@NotEmpty @QueryParam("method") String method,
-            @NotEmpty @QueryParam("user_token") String userToken,
-            @NotEmpty @QueryParam("custom_tag_category_id") String customTagCategoryId,
-            @DefaultValue("1") @Min(1) @QueryParam("index") Integer index,
-            @DefaultValue("10") @Min(1) @Max(100) @QueryParam("size") Integer size) {
-        return customtagListService.customtagListGet(customTagCategoryId,index,size);
-    }
+	/**
+	 * 功能描述：标签列表(分页， 分类名称展示)
+	 * 
+	 * @param customTagCategoryId
+	 * @param index
+	 * @param size
+	 * @return
+	 */
+	@GET
+	@Path("/mkt.customtag.list")
+	public BaseOutput customtagListGet(@NotEmpty @QueryParam("method") String method,
+			@NotEmpty @QueryParam("user_token") String userToken,
+			@NotEmpty @QueryParam("custom_tag_category_id") String customTagCategoryId,
+			@DefaultValue("1") @Min(1) @QueryParam("index") Integer index,
+			@DefaultValue("10") @Min(1) @Max(100) @QueryParam("size") Integer size) {
+		return customtagListService.customtagListGet(customTagCategoryId, index, size);
+	}
+
+	/**
+	 * 功能描述：创建、编辑自定义标签的分类
+	 * 
+	 * @param customTagCategoryId
+	 * @return BaseOutput
+	 */
+	@POST
+	@Path("/mkt.customtag.category.create")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public BaseOutput createCustomtagCategory(@Valid CustomTagCategoryIn body,
+			@Context SecurityContext securityContext) {
+		return customtagCategoryCreateService.updateCustomtagCategory(body, securityContext);
+	}
 
 }
