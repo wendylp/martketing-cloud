c2376558ba9bd6e6f35137ffffa294865a9594c3
shuiyangyang <shuiyangyang@rongcapital.cn>
2017-01-23 14:18:39 +0800

    重写hashCode,toString,equals,构造方法。

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TagSegmentFuzzyListServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/TagSegmentFuzzyListServiceImpl.java
@@ -8,8 +8,6 @@ import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import cn.rongcapital.mkt.common.constant.ApiConstant;
--- marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/out/CustomtagQrcodeFuzzyListOut.java
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/out/CustomtagQrcodeFuzzyListOut.java
@@ -62,6 +62,36 @@ public class CustomtagQrcodeFuzzyListOut {
                 + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((customTagCategoryId == null) ? 0 : customTagCategoryId.hashCode());
+        result = prime * result + ((customTagCategoryName == null) ? 0 : customTagCategoryName.hashCode());
+        result = prime * result + ((customTagId == null) ? 0 : customTagId.hashCode());
+        result = prime * result + ((customTagName == null) ? 0 : customTagName.hashCode());
+        return result;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        CustomtagQrcodeFuzzyListOut other = (CustomtagQrcodeFuzzyListOut) obj;
+        if (customTagCategoryId == null) {
+            if (other.customTagCategoryId != null) return false;
+        } else if (!customTagCategoryId.equals(other.customTagCategoryId)) return false;
+        if (customTagCategoryName == null) {
+            if (other.customTagCategoryName != null) return false;
+        } else if (!customTagCategoryName.equals(other.customTagCategoryName)) return false;
+        if (customTagId == null) {
+            if (other.customTagId != null) return false;
+        } else if (!customTagId.equals(other.customTagId)) return false;
+        if (customTagName == null) {
+            if (other.customTagName != null) return false;
+        } else if (!customTagName.equals(other.customTagName)) return false;
+        return true;
+    }
 
 }
