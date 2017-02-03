48e9540acb3740facf0b095c67393a166c0bc5dc
xiexiaoliang <xiexiaoliang@rongcapital.cn>
2017-02-03 14:31:14 +0800

    注释说明

--- marketing-tool-service/src/main/java/cn/rongcapital/mkt/dataauth/service/DataAuthService.java
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/dataauth/service/DataAuthService.java
@@ -15,19 +15,19 @@ public interface DataAuthService {
     
     /**
      * 新增数据权限
-     * @功能描述: message
-     * @param orgId
-     * @param resourceType
-     * @param resourceId void
+     * @功能描述: 新增数据权限
+     * @param orgId 组织ID
+     * @param resourceType 相应的数据表名
+     * @param resourceId 资源ID
      * @author xie.xiaoliang
      * @since 2017-01-30
      */
     void put(long orgId,String resourceType,long resourceId);
     /**
      * 删除数据权限
-     * @功能描述: message
-     * @param resourceType
-     * @param resourceId void
+     * @功能描述: 删除数据权限
+     * @param resourceType 相应的数据表名
+     * @param resourceId 资源ID
      * @author xie.xiaoliang
      * @since 2017-01-30
      */
@@ -35,9 +35,14 @@ public interface DataAuthService {
     
     /**
      * 分享数据权限
-     * @功能描述: message void
+     * @功能描述: 分享数据权限
+     * @param resourceType 相应数据表名
+     * @param resourceId 主键ID
+     * @param fromOrgId 从哪个组织分享来的
+     * @param toOrgId 分享给哪个组织
+     * @param writeable 是否可写权限
      * @author xie.xiaoliang
-     * @since 2017-01-30
+     * @since 2017-02-03
      */
     void share(String resourceType,long resourceId,long fromOrgId,long toOrgId,boolean writeable);
     
@@ -52,22 +57,28 @@ public interface DataAuthService {
      * @author xie.xiaoliang
      * @since 2017-01-30
      */
+    @Deprecated
     void unshare(String shareType,String resourceType,long resourceId,long fromOrgId,long toOrgId);
     /**
      * 
      * @功能描述: 通过shareId删除数据权限
-     * @param shareId void
+     * @param shareId 分享ID
      * @author xie.xiaoliang
      * @since 2017-01-30
      */
     void unshare(String shareId);
     
     /**
-     * clone数据权限
-     * @功能描述: message void
+     * 克隆数据权限
+     * @功能描述: 克隆数据权限
+     * @param resourceType 相应的数据表名
+     * @param resourceId 主键ID
+     * @param fromOrgId 从哪个组织克隆
+     * @param fromResourceId 从哪个数据ID克隆
+     * @param toOrgId 克隆给哪个组织
+     * @param writeable 可写权限
      * @author xie.xiaoliang
-     * @param fromResourceId TODO
-     * @since 2017-01-30
+     * @since 2017-02-03
      */
     void clone(String resourceType,long resourceId,long fromOrgId,long fromResourceId,long toOrgId,boolean writeable);
     
