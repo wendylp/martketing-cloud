820671046329f5476b7e5ee417fb65a8a47b49c7
guozhenchao <guozhenchao@guozhenchao-PC>
2017-02-06 11:36:50 +0800

    132

--- marketing-tool-dao/src/test/java/cn/rongcapital/mkt/dao/SmsTempletDaoTest.java
+++ marketing-tool-dao/src/test/java/cn/rongcapital/mkt/dao/SmsTempletDaoTest.java
@@ -1,6 +1,7 @@
 package cn.rongcapital.mkt.dao;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -49,6 +50,54 @@ public class SmsTempletDaoTest extends AbstractUnitTest {
         }*/
         
     }
+    
+    //组织id存在
+    @Test
+    public void testSelectByIdAndOrgId01(){
+    	 Map<String, Integer> map = new HashMap<String, Integer>();
+         map.put("id", 1);
+         map.put("orgId", 13);
+         List<SmsTemplet> sms = smsTempletDao.selectByIdAndOrgId(map);
+         Assert.assertEquals(Integer.valueOf(1), sms.get(0).getId());
+    }
+    
+    //组织id存在,资源id 为null
+    @Test
+    public void testSelectByIdAndOrgId02(){
+    	 Map<String, Integer> map = new HashMap<String, Integer>();
+         map.put("orgId", 13);
+         List<SmsTemplet> sms = smsTempletDao.selectByIdAndOrgId(map);
+         Assert.assertTrue(sms.size() == 0);
+    }
+    //组织id为null
+    @Test
+    public void testSelectByIdAndOrgId03(){
+    	 Map<String, Integer> map = new HashMap<String, Integer>();
+    	 map.put("id", 1);
+         List<SmsTemplet> sms = smsTempletDao.selectByIdAndOrgId(map);
+         Assert.assertTrue(sms.size() == 0);
+    }
+    
+    //资源id 不存在
+    @Test
+    public void testSelectByIdAndOrgId04(){
+    	 Map<String, Integer> map = new HashMap<String, Integer>();
+    	 map.put("id", 1000);
+    	 map.put("orgId", 13);
+         List<SmsTemplet> sms = smsTempletDao.selectByIdAndOrgId(map);
+         Assert.assertTrue(sms.size() == 0);
+    }
+    
+    //组织id 不存在
+    @Test
+    public void testSelectByIdAndOrgId05(){
+    	 Map<String, Integer> map = new HashMap<String, Integer>();
+    	 map.put("id", 1);
+    	 map.put("orgId", 1000);
+         List<SmsTemplet> sms = smsTempletDao.selectByIdAndOrgId(map);
+         Assert.assertTrue(sms.size() == 0);
+    }
+    
     
     @Test
     public void testSelectList() {
