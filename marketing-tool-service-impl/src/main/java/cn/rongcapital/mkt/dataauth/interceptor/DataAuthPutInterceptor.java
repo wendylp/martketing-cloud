8e3f35f3cfa35c226a5c47fee573ae0a2fd240a3
zhuxuelong <zhuxuelong@ruixuesoft.com>
2017-02-10 15:06:57 +0800

    data put实现更新

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/dataauth/interceptor/DataAuthPutInterceptor.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/dataauth/interceptor/DataAuthPutInterceptor.java
@@ -1,12 +1,11 @@
 /*************************************************
- * @功能及特点的描述简述: 数据权限创建aop切面类
- * 该类被编译测试过
+ * @功能及特点的描述简述: 数据权限创建aop切面类 该类被编译测试过
  * @see （与该类关联的类）：
  * @对应项目名称：营销云系统
  * @author: 谢小良
  * @version: 版本v1.6
- * @date(创建、开发日期)：2017-02-06 
- * @date(最后修改日期)：2017-02-06 
+ * @date(创建、开发日期)：2017-02-06
+ * @date(最后修改日期)：2017-02-06
  * @复审人：
  *************************************************/
 package cn.rongcapital.mkt.dataauth.interceptor;
@@ -20,6 +19,9 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,44 +30,82 @@ import cn.rongcapital.mkt.dataauth.service.DataAuthService;
 @Aspect
 @Component
 public class DataAuthPutInterceptor {
+
     @Autowired
     private DataAuthService dataAuthService;
-    
+
     // service层切入点
     @Pointcut("@annotation(cn.rongcapital.mkt.dataauth.interceptor.DataAuthPut)")
     public void putServiceAspect() {}
 
-    @AfterReturning("putServiceAspect()")
+    @AfterReturning(pointcut = "putServiceAspect()", returning = "result")
     @Transactional
-    public void doAfterMethod(JoinPoint joinPoint) throws Throwable {
+    public void doAfterMethod(JoinPoint joinPoint, Object result) throws Throwable {
         Method method = ExpressionHelper.getMethod(joinPoint);
         if (method != null && method.isAnnotationPresent(DataAuthPut.class)) {
             DataAuthPut annotation = method.getAnnotation(DataAuthPut.class);
+
             String resourceIdTemp = annotation.resourceId();
             String orgIdTemp = annotation.orgId();
-            
-            
+            String outputResourceId = annotation.outputResourceId();
             String resourceType = annotation.resourceType();
-            Long resourceIdObj =null;
+
+            // 参数校验
+            if (StringUtils.isBlank(orgIdTemp)) {
+                throw new IllegalArgumentException("orgId expression is illegal.");
+            }
+            if (StringUtils.isBlank(outputResourceId)) {
+                throw new IllegalArgumentException("outputResourceId expression is illegal.");
+            }
+            if (StringUtils.isBlank(resourceType)) {
+                throw new IllegalArgumentException("resourceType expression is illegal.");
+            }
+
+            Long resourceIdObj = null;
             Long orgIdObj = null;
-            switch(annotation.type()){
+            switch (annotation.type()) {
                 case SpEl:
-                    resourceIdObj = ExpressionHelper.executeTemplate(resourceIdTemp, joinPoint,Long.class);
-                    orgIdObj = ExpressionHelper.executeTemplate(orgIdTemp, joinPoint,Long.class);
+                    if (StringUtils.isNotBlank(resourceIdTemp)) {
+                        resourceIdObj = ExpressionHelper.executeTemplate(resourceIdTemp, joinPoint, Long.class);
+                    }
+                    orgIdObj = ExpressionHelper.executeTemplate(orgIdTemp, joinPoint, Long.class);
                     break;
                 default:
-                    resourceIdObj =Long.parseLong(resourceIdTemp);
+                    if (StringUtils.isNotBlank(resourceIdTemp)) {
+                        resourceIdObj = Long.parseLong(resourceIdTemp);
+                    }
                     orgIdObj = Long.parseLong(orgIdTemp);
                     break;
-                }
-            //参数校验
-            if(StringUtils.isBlank(resourceType )){
-                throw new NoSuchElementException();
             }
-            long resourceId = resourceIdObj.longValue();
-            long orgId =  orgIdObj.longValue();
-            //将新增数据保存对应的数据权限
-            this.dataAuthService.put(orgId, resourceType, resourceId);
+
+            // 数据更新情况不需要生成权限
+            if (resourceIdObj != null && resourceIdObj.longValue() > 0) {
+                return;
+            }
+            if (orgIdObj == null || orgIdObj.longValue() == 0) {
+                throw new NoSuchElementException("calculate orgId from expression is illegal.");
+            }
+            // 没有输出的情况
+            if (result == null) {
+                throw new NoSuchElementException("business method has no output.");
+            }
+            Long outputId = null;
+            switch (annotation.type()) {
+                case SpEl:
+                    ExpressionParser parser = new SpelExpressionParser();
+                    StandardEvaluationContext context = new StandardEvaluationContext(result);
+                    outputId = parser.parseExpression(outputResourceId).getValue(context, Long.class);
+                    break;
+                default:
+                    outputId = Long.parseLong(outputResourceId);
+                    break;
+            }
+            // 输出资源ID不合法的情况
+            if (outputId == null || outputId.longValue() == 0) {
+                throw new NoSuchElementException("calculate generated resource_id from expression is illegal.");
+            }
+            // 将新增数据保存对应的数据权限
+            this.dataAuthService.put(orgIdObj.longValue(), resourceType, outputId.longValue());
         }
     }
 }
