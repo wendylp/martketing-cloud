95c95648c3575f84088eda926b92e2462c6755cc
zhuxuelong <zhuxuelong@ruixuesoft.com>
2017-04-12 18:34:04 +0800

    用户导入人群列表用作活动的目标人群

---
+++ marketing-tool-vo/src/main/java/cn/rongcapital/mkt/vo/in/AudienceCreateIn.java
@@ -0,0 +1,65 @@
+/*************************************************
+ * @功能简述: 保存固定人群
+ * @项目名称: marketing cloud
+ * @see:
+ * @author: zhuxuelong
+ * @version: 0.0.1
+ * @date: 2017/4/12
+ * @复审人:
+ *************************************************/
+package cn.rongcapital.mkt.vo.in;
+
+import java.util.List;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class AudienceCreateIn {
+
+    /**
+     * 人群名称
+     */
+    @NotEmpty
+    private String name;
+
+    /**
+     * 来源
+     */
+    @NotEmpty
+    private String source;
+
+    /**
+     * 人群明细
+     */
+    @NotEmpty
+    private List<Integer> details;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public List<Integer> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<Integer> details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "AudienceCreateIn [name=" + name + ", source=" + source + ", details=" + details + "]";
+    }
+
+}
