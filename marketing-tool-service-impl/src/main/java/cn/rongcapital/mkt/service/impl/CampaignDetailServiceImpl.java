6bd149c699fa824904a2fb60887568719d7c1cf4
LiuQ <liuqi@rongcapital.cn>
2017-04-21 19:19:27 +0800

    fix bug

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignDetailServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/service/impl/CampaignDetailServiceImpl.java
@@ -384,7 +384,7 @@ public class CampaignDetailServiceImpl implements CampaignDetailService {
 
 		String key = this.createKey(campaignId, itemId);
 		CampaignDetail detail = cache.get(key);
-		if (detail.getIsHaveSubTable().intValue() != HAVE_SUB_DATA) {
+		if (detail != null && detail.getIsHaveSubTable().intValue() != HAVE_SUB_DATA) {
 			detail.setIsHaveSubTable(HAVE_SUB_DATA);
 			this.updateCampaignDetailSubTableStatus(campaignId, itemId, HAVE_SUB_DATA);
 		}
