e541dee92f092c9d51243f979f3a9af51d4100c2
xiexiaoliang <xiexiaoliang@rongcapital.cn>
2017-05-03 17:02:48 +0800

    生成活动报表接口调用bug修复

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/bbx/service/impl/BbxCouponCodeAddServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/bbx/service/impl/BbxCouponCodeAddServiceImpl.java
@@ -131,7 +131,7 @@ public class BbxCouponCodeAddServiceImpl implements BbxCouponCodeAddService {
             for (BbxCouponCodeAdd bbxCouponCodeAdd : item) {
                 //仅同步以活动发送出去的优惠券信息，短信任务发送的不进行同步
                 if(bbxCouponCodeAdd.getCampsignId() != null){
-                    this.campaignDetailService.updateCampaignMemberCouponId(bbxCouponCodeAdd.getCampsignId().intValue(),String.valueOf( bbxCouponCodeAdd.getItemId()),Integer.valueOf( bbxCouponCodeAdd.getMainId()), 0, bbxCouponCodeAdd.getCouponId());
+                    this.campaignDetailService.updateCampaignMemberCouponId(bbxCouponCodeAdd.getCampsignId(), bbxCouponCodeAdd.getItemId(),Integer.valueOf( bbxCouponCodeAdd.getMainId()), 1, bbxCouponCodeAdd.getCouponId());
                 }
             }
         }
@@ -203,7 +203,7 @@ public class BbxCouponCodeAddServiceImpl implements BbxCouponCodeAddService {
                         
                       //仅同步以活动发送出去的优惠券信息，短信任务发送的不进行同步
                         if(bbxCouponCodeAdd.getCampsignId() != null){
-                            this.campaignDetailService.updateCampaignMemberCouponId(bbxCouponCodeAdd.getCampsignId().intValue(),String.valueOf( bbxCouponCodeAdd.getItemId()),Integer.valueOf( bbxCouponCodeAdd.getMainId()), 1, bbxCouponCodeAdd.getCouponId());
+                            this.campaignDetailService.updateCampaignMemberCouponStatus(bbxCouponCodeAdd.getCampsignId(),bbxCouponCodeAdd.getItemId(),Integer.valueOf( bbxCouponCodeAdd.getMainId()), 1);
                         }
                     }
                 }
