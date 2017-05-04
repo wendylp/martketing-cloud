c82bb37e4d66c16c902219d8958b50e8fabb8d2d
LiuQ <liuqi@rongcapital.cn>
2017-05-04 11:04:27 +0800

    fix Sync

---
+++ marketing-tool-service/src/main/java/cn/rongcapital/mkt/service/SmsSyncCouponService.java
@@ -0,0 +1,19 @@
+package cn.rongcapital.mkt.service;
+
+import java.util.List;
+
+public interface SmsSyncCouponService {
+	
+	/**
+	 * 处理短信状态:
+	 * 
+	 * @param campaignHeadId
+	 * @param smsTaskHeadId
+	 * @param smsTaskDetailIds
+	 *            无效的短信数据集
+	 * @return
+	 */
+	default public boolean processSmsStatus(Integer campaignHeadId, Long smsTaskHeadId, List<Long> smsTaskDetailIds) {
+		return false;
+	}
+}
