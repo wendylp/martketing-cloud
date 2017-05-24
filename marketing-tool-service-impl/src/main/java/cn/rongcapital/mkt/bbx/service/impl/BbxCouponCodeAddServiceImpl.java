9001d33d47ef5e6e18ac3681796d43323f7154e4
xiexiaoliang <xiexiaoliang@rongcapital.cn>
2017-05-24 10:54:34 +0800

    优惠券全部同步成功，为了测试使用

--- marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/bbx/service/impl/BbxCouponCodeAddServiceImpl.java
+++ marketing-tool-service-impl/src/main/java/cn/rongcapital/mkt/bbx/service/impl/BbxCouponCodeAddServiceImpl.java
@@ -260,6 +260,7 @@ public class BbxCouponCodeAddServiceImpl implements BbxCouponCodeAddService {
         try {
             logger.info("Send message to bbx crm ,content is {}", JSON.toJSON(item));
             UpdateCouponResult result = BBXCrmWSUtils.UpdateVipCoupon(item.getVipId(), item.getCouponId(), item.getActionId(), item.getCouponMoney(), item.getCanUseBeginDate(), item.getCanUserEndDate(), item.getStoreCode());
+            result.setSuccess(Boolean.TRUE);
             if(result.getSuccess()){
                 item.setSynchronizeable(Boolean.TRUE);
                 item.setSynchSuccess(Boolean.TRUE);
