460c2ea68fd658b91db5eb1d515f306a7736408d
liuhaizhan <liuhaizhan@rongcapital.cn>
2017-05-25 15:21:12 +0800

    添加微信返回粉丝信息日志

--- marketing-tool-biz-impl/src/main/java/cn/rongcapital/mkt/biz/impl/WechatMemberBizImpl.java
+++ marketing-tool-biz-impl/src/main/java/cn/rongcapital/mkt/biz/impl/WechatMemberBizImpl.java
@@ -134,6 +134,7 @@ public class WechatMemberBizImpl extends BaseBiz implements WechatMemberBiz {
                 fromIndex = fromIndex + ApiConstant.WEIXIN_BATCH_GET_USER_INFO_SIZE;;
                 size= fromIndex+ApiConstant.WEIXIN_BATCH_GET_USER_INFO_SIZE;
                 String userInfoesStr = WxComponentServerApi.getBaseWxSdk().getBatchGetUserInfoResult(app, openidListTemps);
+                 logger.info("粉丝详情:{},openid:{},原文:{}",app.getId()+"_",openidListTemps,userInfoesStr);
 				/**
 				 * 去掉特殊字符 例如表情符等等
 				 */
@@ -141,6 +142,7 @@ public class WechatMemberBizImpl extends BaseBiz implements WechatMemberBiz {
                 /**
                  * 转成用户对象
                  */
+                  logger.info("粉丝处理过后的详情:{}",userInfoesStr);
     			List<UserInfo> userInfoes = this.getUserInfoesByStr(userInfoesStr);
                 /**
                  * 记入获取微信公众号的粉丝日志
