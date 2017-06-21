a667b184f765e477d71f47c79e03d138aa418438
shanjingqi <shanjingqi@rongcapital.cn>
2017-06-21 14:18:01 +0800

    清理同步贝贝熊ETL优惠券从 Mongo 到MySQL 功能，SyncETLMaterialCouponDataServiceImpl类以及对应测试类

--- marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
+++ marketing-tool-api/src/main/java/cn/rongcapital/mkt/material/coupon/api/CouponApi.java
@@ -50,7 +50,6 @@ import cn.rongcapital.mkt.material.coupon.service.MaterialCouponPageListService;
 import cn.rongcapital.mkt.material.coupon.service.MaterialCouponPutInGeneralService;
 import cn.rongcapital.mkt.material.coupon.service.MaterialCouponReleaseGeneralService;
 import cn.rongcapital.mkt.material.coupon.service.MaterialCouponVerifyGeneralService;
-import cn.rongcapital.mkt.material.coupon.service.SyncETLMaterialCouponDataService;
 import cn.rongcapital.mkt.material.coupon.vo.MaterialCouponCreateAudienceVO;
 import cn.rongcapital.mkt.material.coupon.vo.MaterialCouponDeleteIn;
 import cn.rongcapital.mkt.material.coupon.vo.in.MaterialCouponCodeVerifyIn;
@@ -500,24 +499,6 @@ public class CouponApi {
     }
 	
 	
-	@Autowired
-	private SyncETLMaterialCouponDataService syncETLMaterialCouponDataService;
-	
-    /**
-     * 同步贝贝熊ETL优惠券从 Mongo 到MySQL
-     * 
-     * 接口：mkt.material.coupon.sync
-     * 
-     * @author shanjingqi
-     * @throws JMSException 
-     * @Date 2016-7-17
-     */ 
-    @GET
-    @Path("/mkt.material.coupon.sync")
-    public int sync() {
-        return syncETLMaterialCouponDataService.sync();
-    }
-    
     /**
      * @author shanjingqi
      * @功能简述:优惠券设置stock_total，并生成对应数量的码（贝贝熊）
